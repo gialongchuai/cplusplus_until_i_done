5f830f773ea9aceda13de9825377fc79d42f2da0
gialongchuai <gialongchuai@gmail.com>
2025-04-26 21:19:07 +0700

    galaxy: tong so trong day

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,21 +2,16 @@
 using namespace std;
 
 int main() {
-	// 28tech voi 28h, 28ph va tinh dang o giai doan h:m tinh xem
-	// k phut nua thi o giai doan h:m nao?
-	int a, b, c;
-	cin >> a >> b >> c;
-	int min_val = min({a,b,c});
-	int max_val = max({a,b,c});
-	
-	// Toi uu
-	cout << min_val << " " << (a+b+c - min_val - max_val) << " " << max_val << endl;
-	
-	int middle;
-	if(a != min_val && a != max_val) middle = a;
-	if(b != min_val && b != max_val) middle = b;
-	if(c != min_val && c != max_val) middle = c;
-	cout << min_val << " " << middle << " " << max_val;
+	// tong so trong day 
+	int n; cin >> n;
+	int sum = 0;
+	for(int i = 0; i<n; i++) {
+		char c; cin >> c;
+		if(c>='0' && c<='9') {
+			sum += c - '0';
+		}
+	}	
+	cout << sum;
 	
 	return 0;
 }
