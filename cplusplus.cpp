f31d5531ba9954ec97c70e1f282ed960ad4583c0
gialongchuai <gialongchuai@gmail.com>
2025-04-26 21:14:44 +0700

    galaxy: sort 3 so nguyen chay

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -4,13 +4,19 @@ using namespace std;
 int main() {
 	// 28tech voi 28h, 28ph va tinh dang o giai doan h:m tinh xem
 	// k phut nua thi o giai doan h:m nao?
-	int h, m, k;
-	cin >> h >> m >> k;
-	k %= 28 * 28; // vuot qua 1 ngay thi chi lay phan phut' du
-	int time = h * 28 + m + k;
-	time %= 28*28;
-	int gio = time / 28, phut = time % 28;
-	cout << setw(2) << setfill('0') << "h: " << h << setw(2) << setfill('0') << " m: " << phut;
-
+	int a, b, c;
+	cin >> a >> b >> c;
+	int min_val = min({a,b,c});
+	int max_val = max({a,b,c});
+	
+	// Toi uu
+	cout << min_val << " " << (a+b+c - min_val - max_val) << " " << max_val << endl;
+	
+	int middle;
+	if(a != min_val && a != max_val) middle = a;
+	if(b != min_val && b != max_val) middle = b;
+	if(c != min_val && c != max_val) middle = c;
+	cout << min_val << " " << middle << " " << max_val;
+	
 	return 0;
 }
