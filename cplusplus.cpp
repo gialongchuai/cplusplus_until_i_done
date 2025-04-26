0ba6549420cda95ea5a721727a14b905c58d79ae
gialongchuai <gialongchuai@gmail.com>
2025-04-26 21:21:09 +0700

    galaxy: giai thich them ...

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -8,7 +8,8 @@ int main() {
 	for(int i = 0; i<n; i++) {
 		char c; cin >> c;
 		if(c>='0' && c<='9') {
-			sum += c - '0';
+			sum += c - '0'; // neu khong -0 thi no se lay vi du 0 48, 1 49,
+							// nen 49 - '0'  = 49 - 48 = 1
 		}
 	}	
 	cout << sum;
