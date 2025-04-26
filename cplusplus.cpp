165491a54628638a8ec5cdf7562e6017b35509b5
gialongchuai <gialongchuai@gmail.com>
2025-04-26 17:47:38 +0700

    galaxy: lam tron so not sp

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,19 +2,13 @@
 using namespace std;
 
 int main() {
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
+	// 4.9 => 5 // 4.2 => 4
+	double n; cin >> n;
+	if(n - (int)n >= 0.5) {
+		cout << (int)n + 1 << endl;
+	} else {
+		cout << (int)n << endl;
+	}
 	
-	int max_val = a;
-	if(b > max_val) max_val = b;
-	if(c > max_val) max_val = c;
-	if(d > max_val) max_val = d;
-	
-	int min_val = a;
-	if(b < min_val) min_val = b;
-	if(c < min_val) min_val = c;
-	if(d < min_val) min_val = d;
-	
-	cout << "min: " << min_val  << ", max: " << max_val << endl;
 	return 0;
 }
