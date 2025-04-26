8712d28e2193621e83734b67891947ac6484b3c8
gialongchuai <gialongchuai@gmail.com>
2025-04-26 19:52:11 +0700

    galaxy: gio phut giay voi n(s) cho truoc

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,20 +2,13 @@
 using namespace std;
 
 int main() {
-	// tinh tong va dem chu so nguyen: n = 7832 => 10 : 4
-	// 7 / 10 = 0
-	int sum = 0, cnt =0;
-	int n; cin >> n;
-	if(n==0) {
-		cout << 0 << " " << 1;
-	} else {
-			while(n!=0) {
-			sum+=n%10;
-			n/=10;
-			cnt++;
-		}
-		cout << sum << " " << cnt;
-	}
+	// tinh h, tinh p, tinh s cua so nguyen la n(s) cho truoc
+	// 
+	long long n; cin >> n;
+	long long gio = n / 3600;
+	long long phut = (n%3600) / 60;
+	long long giay = ((n%3600)%60); // giay = n % 60
+	cout << "h: " << gio << " p: " << phut << " s: " << giay;
 
 	return 0;
 }
