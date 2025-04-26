864632e7e4d2a4ef521958ecb1bc90037cfb4484
gialongchuai <gialongchuai@gmail.com>
2025-04-26 17:23:33 +0700

    galaxy: tinh to tien

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 
 int main() {
-	int a = 10 , b = 5, c =9;
+	// 1, 5, 10, 20, 100
+	// 1859
+	int n; cin >> n;
 	
-	cout << min({a,b,c}) << endl; // 5
-	cout << max({a,b,c}) << endl; // 10
+	int r = n / 100;
+	cout << "100 : " << r << endl;
+	n %= 100;
 	
+	int l = n / 20;
+	cout << "20 : " << l << endl;
+	n %= 20;
+	
+	int f = n / 10;
+	cout << "10 : " << f << endl;
+	n %= 10;
+	
+	int i = n / 5;
+	cout << "5 : " << i << endl;
+	n %= 5;
+	
+	int h = n / 1;
+	cout << "1 : " << h << endl;
 	return 0;
 }
