3387574645a47dcc48578df536377ce6c363d4b7
gialongchuai <gialongchuai@gmail.com>
2025-04-27 15:49:19 +0700

    galaxy: so dau tien lon nhat cua so nguyen

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,16 +2,36 @@
 using namespace std;
 
 int main() {
-	// chu so lon nhat : 2829292 => 9
+	// chu so dau tien lon nhat : 9012019 => YES
 	int n; cin >> n;
 	int max_digit = n % 10;
 	int r;
-	while(n!=0) {
+	// Toi uu 
+	while(n>=10) {
 		r = n % 10;
 		if(r > max_digit) max_digit = r;
 		n/=10;
 	}
-	cout << max_digit << endl;
+	if(n >= max_digit) {
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+	
+	// lan nao lap cung phai? xet dien kien
+	while(n>=10) {
+		r = n % 10;
+		if(r > max_digit) max_digit = r;
+		n/=10;
+		if(n>=0 && n <=9) {
+			cout << n << endl;
+			if(n >= max_digit) {
+				cout << "YES";
+			} else {
+				cout << "NO";
+			}
+		}
+	}
 	
 	return 0;
 }
