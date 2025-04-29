8488cd899452e9918d7e8a0a0a2394ab7c2295fd
gialongchuai <gialongchuai@gmail.com>
2025-04-29 19:09:58 +0700

    galaxy: mang chua 92 so fibonacci

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,34 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool snt(int n) {
-	if(n<2) return false;
-	for(int i = 2; i*i<=n; i++){
-		if(n%i==0) return false;
-	}
-	return true;
-}
-
-int main() { // cout thu tu cung voi tan so xuat hien cua 1 so trong day
-	int n; cin >> n;
-	int a[n];
-	for(int i = 0; i<n; i++) {
-		cin >> a[i];
+int main() { // cout list 92 numbers fibonacci
+	long long fibo[100];
+	fibo[0] = 0, fibo[1] = 1;
+	
+	for(int i = 2; i<=92; i++) {
+		fibo[i] = fibo[i-2] + fibo[i-1];
 	}
 	
-	for(int i = 0; i < n; i++) {
-		bool flag = true;
-		int cnt = 1;
-		for(int j = i - 1; j >= 0; j--) {
-			if(a[i] == a[j]) flag = false;
-		}
-		if(flag) {
-			for(int j = i+1; j<n; j++) {
-				if(a[i] == a[j]) ++cnt;
-			}
-			cout << a[i] << " " << cnt;
-			cout << endl;
-		}
+	for(int i = 0; i<=92; i++) {
+		cout << fibo[i] << " ";
 	}
 		
 	return 0;
