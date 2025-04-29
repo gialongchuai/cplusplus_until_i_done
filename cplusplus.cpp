f10bb067c0570aa3a048c5e0e0dffc1503d746da
gialongchuai <gialongchuai@gmail.com>
2025-04-29 16:17:44 +0700

    galaxy: mang doi xung va lat nguoc mang

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,24 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // bp count min trong day
+bool mang_doi_xung_01(int a[], int n) {
+	for(int i = 0; i<n/2; i++) {
+		if(a[i]!=a[n-1- i]) return false;
+	}
+	return true;
+}
+
+bool mang_doi_xung_02(int a[], int n) {
+	int l = 0, r = n - 1;
+	while(l<r) {
+		if(a[l]!=a[r]) return false;
+		l++, r--;
+	}
+	return true;
+}
+
+void lat_nguoc(int a[], int n) {
+	int l = 0, r = n-1;
+	int temp;
+	while(l < r){
+		temp = a[l];
+		a[l] = a[r];
+		a[r] = temp;
+		l++, r--;
+	}
+}
+
+int main() { // mang doi xung + lat nguoc mang
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	int min_val = a[0], cnt = 0;
-	for(int i = 0; i<n; i++) {
-		if(a[i] < min_val){
-			min_val = a[i];
-			cnt = 1;
-		} else if(a[i] == min_val) {
-			++cnt;
-		}
+	cout << mang_doi_xung_01(a,n) << endl;
+	cout << mang_doi_xung_02(a,n) << endl;
+	
+	lat_nguoc(a,n);
+	for(int x : a) {
+		cout << x << " ";
 	}
-	
-	cout << cnt;
-	
 	return 0;
 }
 
