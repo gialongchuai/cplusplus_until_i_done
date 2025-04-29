fff7ff678ffeb8e13d84540820c50981340e793e
gialongchuai <gialongchuai@gmail.com>
2025-04-29 16:49:25 +0700

    galaxy: phan tu chan o vi tri chan trong day so

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // count lon hon, nho hon x
+int main() { // cout phan tu chan o vi tri chan
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	int x; cin >> x;
-	int s_x = 0, l_x = 0;
-	for(int i : a) {
-		if(i < x) s_x++;
-		if(i > x) l_x++;
-	}
 	
-	cout << s_x << " " << l_x;
+	int flag = true;
+	for(int i = 0 ; i < n; i++) {
+		if(i%2==0&&a[i]%2==0) {
+			cout << a[i] << " ";
+			flag = false;
+		}
+	}
+	if(flag) {
+		cout << "NONE";
+	}
+		
 	return 0;
 }
 
