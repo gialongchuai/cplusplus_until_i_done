9a610de7c07bfc773e33e5835bed3f1bdfbebcfa
gialongchuai <gialongchuai@gmail.com>
2025-04-29 20:35:36 +0700

    galaxy: 2 cach dung mang danh dau voi so xuat hien cung tan suat tuong ung

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int cnt[1000001];
 
-int main() { // mang danh dau dem so phan tu khac nhau trong mang
+int main() { // mang danh dau
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
@@ -12,18 +12,20 @@ int main() { // mang danh dau dem so phan tu khac nhau trong mang
 	
 	int max_val = a[0];
 	for(int i = 0; i<n; i++) {
-		cnt[a[i]] = 1;
+		cnt[a[i]]++;
 		if(a[i] > max_val) max_val = a[i];
 	}
 	
-	for(int i = 0; i<= 1000000; i++) { // in cac so khac nhau
-		if(cnt[i] == 1) cout << i << " ";
+	for(int i = 0; i<= max_val; i++) { // in theo thu tu tang dan cung voi tan so xuat hien
+		if(cnt[i] != 0) {
+			cout << i << " " << cnt[i] << endl; 	
+		}
 	}
-	cout << endl;
+	cout << "============" << endl;
 	
-	for(int i = 0; i <n; i++) { // in cac so khac nhau theo thu tu xuat hien
-		if(cnt[a[i]] == 1) {
-			cout << a[i] << " ";
+	for(int i = 0; i<n; i++) { // in theo thu tu xuat hien cung voi tan suat tuong ung
+		if(cnt[a[i]] != 0) {
+			cout << a[i] << " " << cnt[a[i]] << endl;
 			cnt[a[i]] = 0;
 		}
 	}
