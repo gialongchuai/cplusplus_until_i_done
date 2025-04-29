99835a18d403043eb83066e88dc60bf40bac4589
gialongchuai <gialongchuai@gmail.com>
2025-04-29 16:35:48 +0700

    galaxy: dem so luong lon hon va nho hon cua gia tri x

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,46 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool mang_doi_xung_01(int a[], int n) {
-	for(int i = 0; i<n/2; i++) {
-		if(a[i]!=a[n-1- i]) return false;
-	}
-	return true;
-}
-
-bool mang_doi_xung_02(int a[], int n) {
-	int l = 0, r = n - 1;
-	while(l<r) {
-		if(a[l]!=a[r]) return false;
-		l++, r--;
-	}
-	return true;
-}
-
-void lat_nguoc(int a[], int n) {
-	int l = 0, r = n-1;
-	int temp;
-	while(l < r){
-		temp = a[l];
-		a[l] = a[r];
-		a[r] = temp;
-		l++, r--;
-	}
-}
-
-int main() { // mang doi xung + lat nguoc mang
+int main() { // count lon hon, nho hon x
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	cout << mang_doi_xung_01(a,n) << endl;
-	cout << mang_doi_xung_02(a,n) << endl;
-	
-	lat_nguoc(a,n);
-	for(int x : a) {
-		cout << x << " ";
+	int x; cin >> x;
+	int s_x = 0, l_x = 0;
+	for(int i : a) {
+		if(i < x) s_x++;
+		if(i > x) l_x++;
 	}
+	
+	cout << s_x << " " << l_x;
 	return 0;
 }
 
