0c1ffd82fdb48e3d9aaf484c1999faa59fc84930
gialongchuai <gialongchuai@gmail.com>
2025-04-29 17:46:21 +0700

    galaxy: do chenh lech giua 2 vi tri nho nhat

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // cout so lan va tong cac vi tri bang so x
+int main() { // do chenh lech nho nhat giua 2 vi tri trong mang
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	int x; cin >> x;
-	int cnt = 0;
-	for(int i = 0 ; i < n - 1; i++) { // chuan luon 3 8 9 2 4 7 chi can toi 4 la du?
+
+	int distance_min = abs(a[0]-a[1]); // co the dung int_max sau do trong for 2 gan lai bang min(a,b)
+	for(int i = 0 ; i < n - 1; i++) { 
 		for(int j = i + 1; j < n; j++) {
-			if(a[i] + a[j] == x) {
-				cout << a[i] << " " << a[j] << endl;
-				cnt++;
-			}
+			int e = abs(a[i] - a[j]);
+			if(e < distance_min) distance_min = e;
 		}
 	}
-	cout << cnt;
+	cout << distance_min;
 		
 	return 0;
 }
