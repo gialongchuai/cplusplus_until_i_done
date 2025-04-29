60adc6d63e4fb20d0305f20c0abf31d3ef45ef63
gialongchuai <gialongchuai@gmail.com>
2025-04-29 14:06:38 +0700

    galaxy: min max gia tri trong day

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,34 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool snt(int n) {
-	if(n<2) return false;
-	for(int i = 2; i*i <=n; i++) {
-		if(n%i==0) return false;
-	}
-	return true;
-}
-
-int main() { // avg cac so nguyen to trong array // round 3 chu so thap phan
-			/*
-				5
-				-911 234 151 347 231
-				=>249.000
-			*/
+int main() { // min max trong day
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	int cnt = 0, tong = 0;
+	
+	int min_val = a[0], max_val = a[0];
 	for(int x : a) {
-		if(snt(x)) {
-			tong+=x;
-			cnt++;
-		}
+		if(x>max_val) max_val = x;
+		if(x<min_val) min_val = x;
 	}
-	double avg = 1.0 * tong / cnt;
-	cout << fixed << setprecision(3) << avg;
+	cout << min_val << " " << max_val << endl;
+	
+	//bp : do ton them 1 vong lap duyen 0
+	for(int i = 1; i<n; i++) {
+		if(a[i]>max_val) max_val = a[i];
+		if(a[i]<min_val) min_val = a[i];
+	}
+	cout << min_val << " " << max_val;
 	
 	return 0;
 }
