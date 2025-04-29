8ea980448e382e4be760cd569105a0163665ddba
gialongchuai <gialongchuai@gmail.com>
2025-04-29 20:00:07 +0700

    galaxy: bp mang danh dau so phan tu khac nhau trong mang

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -10,12 +10,14 @@ int main() { // mang danh dau dem so phan tu khac nhau trong mang
 		cin >> a[i];
 	}
 	
+	int max_val = a[0];
 	for(int i = 0; i<n; i++) {
 		cnt[a[i]] = 1;
+		if(a[i] > max_val) max_val = a[i];
 	}
 	
 	int dem = 0;
-	for(int i = 0; i<= 1000000; i++) {
+	for(int i = 0; i<= max_val; i++) {
 		if(cnt[i] == 1) ++dem;
 	}
 	cout << dem;
