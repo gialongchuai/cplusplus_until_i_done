1b0c6c3b6fe3e9864b24ca3ccec8b3cd5fb60428
gialongchuai <gialongchuai@gmail.com>
2025-04-29 11:04:14 +0700

    galaxy: bp bai toan tinh dong du cua day so

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main() {
 	int n; cin >> n; // bai toan dong du (a+b) % c = (a%c+b%c) %c
+					// bp : cout << ((a%c) % c + (b%c)) % c ...
 	int a = 1e9 + 7;
 	long long c;
 	long long sum = 0;
 	for(int i = 0; i<n; i ++) {
 		cin >> c;
 		sum+= c%a;
+		sum%=a;
 	}
-	cout << sum%a;
+	cout << sum;
 	return 0;
 }
 
