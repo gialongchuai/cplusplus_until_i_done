cfac189cad880505bbf680892378b7b2cca02a8b
gialongchuai <gialongchuai@gmail.com>
2025-04-29 13:57:56 +0700

    galaxy: trung binh cong cac so nguyen to trong day

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,32 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // so phan tu chan, le; tong phan tu chan, le
-			//5
-			// 301 201 302 18 9
-			// => {2 320
-			// 	   3 511}
+bool snt(int n) {
+	if(n<2) return false;
+	for(int i = 2; i*i <=n; i++) {
+		if(n%i==0) return false;
+	}
+	return true;
+}
+
+int main() { // avg cac so nguyen to trong array // round 3 chu so thap phan
+			/*
+				5
+				-911 234 151 347 231
+				=>249.000
+			*/
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	
-	int dem_l = 0, dem_c = 0;
-	int tong_l = 0, tong_c = 0;
-	
-	for(int i = 0; i<n; i++) {
-		if(a[i]%2==0){
-			tong_c+= a[i];
-			++dem_c;
-		} else {
-			tong_l+= a[i];
-			++dem_l;
+	int cnt = 0, tong = 0;
+	for(int x : a) {
+		if(snt(x)) {
+			tong+=x;
+			cnt++;
 		}
 	}
-	
-	cout << dem_c << " " << tong_c << endl;
-	cout << dem_l << " " << tong_l << endl;
+	double avg = 1.0 * tong / cnt;
+	cout << fixed << setprecision(3) << avg;
 	
 	return 0;
 }
