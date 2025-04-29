accbaee94e68852c1aa0138439e3010aaa31f5ba
gialongchuai <gialongchuai@gmail.com>
2025-04-29 14:24:17 +0700

    galaxy: dem min gia tri trong day

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // min max trong day
+int main() { // count min trong day
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	
-	int min_val = a[0], max_val = a[0];
-	for(int x : a) {
-		if(x>max_val) max_val = x;
-		if(x<min_val) min_val = x;
-	}
-	cout << min_val << " " << max_val << endl;
-	
-	//bp : do ton them 1 vong lap duyen 0
+	int min_val = a[0];
 	for(int i = 1; i<n; i++) {
-		if(a[i]>max_val) max_val = a[i];
-		if(a[i]<min_val) min_val = a[i];
+		if(a[i] < min_val) min_val = a[i];
 	}
-	cout << min_val << " " << max_val;
+	
+	int cnt = 0;
+	for(int x : a) {
+		if(x==min_val) cnt++;
+	}
+	
+	cout << cnt;
 	
 	return 0;
 }
