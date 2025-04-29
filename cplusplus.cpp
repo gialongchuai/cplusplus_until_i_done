eb45a8434b4044ffcc8befb07dfbe6c391ac9eea
gialongchuai <gialongchuai@gmail.com>
2025-04-29 18:00:50 +0700

    galaxy: thu tu xuat hien cac phan tu khac nhau trong day

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // do chenh lech nho nhat giua 2 vi tri trong mang
+int main() { // cout cac phan tu khac nhau theo vi tri xuat hien
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
 
-	int distance_min = abs(a[0]-a[1]); // co the dung int_max sau do trong for 2 gan lai bang min(a,b)
-	for(int i = 0 ; i < n - 1; i++) { 
-		for(int j = i + 1; j < n; j++) {
-			int e = abs(a[i] - a[j]);
-			if(e < distance_min) distance_min = e;
+	for(int i = 0 ; i < n; i++) {
+		int flag = true;
+		for(int j = i - 1 ; j >= 0; j--) {
+			if(a[i] == a[j]) flag = false;
 		}
+		if(flag) cout << a[i] << " ";
 	}
-	cout << distance_min;
 		
 	return 0;
 }
