4a7f7796ea54aaa71693f5165c144d7bf87541c0
gialongchuai <gialongchuai@gmail.com>
2025-04-29 18:32:48 +0700

    galaxy: tong so ben trai va phai phai la 1 so nguyen to

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,19 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // cout cac phan tu khac nhau theo vi tri xuat hien
+bool snt(int n) {
+	if(n<2) return false;
+	for(int i = 2; i*i<=n; i++){
+		if(n%i==0) return false;
+	}
+	return true;
+}
+
+int main() { // cout vi tri phan tu co tong ben trai va tong ben phai la snt
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-
-	for(int i = 0 ; i < n; i++) {
-		int flag = true;
-		for(int j = i - 1 ; j >= 0; j--) {
-			if(a[i] == a[j]) flag = false;
+	
+	for(int i = 1 ; i < n - 1; i++) {
+		int sum_trai = 0, sum_phai = 0;
+		for(int j = i + 1; j < n; j++) {
+			sum_phai+=a[j];
 		}
-		if(flag) cout << a[i] << " ";
+		for(int j = i - 1; j>=0; j--) {
+			sum_trai+=a[j];
+		}
+		if(snt(sum_trai) && snt(sum_phai)) {
+			cout << i << " ";
+		}
 	}
 		
 	return 0;
