6ef55283f7fff361b20fad4ca123ae04a7ec4b92
gialongchuai <gialongchuai@gmail.com>
2025-04-29 14:32:41 +0700

    galaxy: bp dem min gia tri trong day

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // count min trong day
+int main() { // bp count min trong day
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	int min_val = a[0];
-	for(int i = 1; i<n; i++) {
-		if(a[i] < min_val) min_val = a[i];
-	}
-	
-	int cnt = 0;
-	for(int x : a) {
-		if(x==min_val) cnt++;
+	int min_val = a[0], cnt = 0;
+	for(int i = 0; i<n; i++) {
+		if(a[i] < min_val){
+			min_val = a[i];
+			cnt = 1;
+		} else if(a[i] == min_val) {
+			++cnt;
+		}
 	}
 	
 	cout << cnt;
