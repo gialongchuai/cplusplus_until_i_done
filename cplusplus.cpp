a09c743ec031e55d11da3cf693881e6bdf5632fe
gialongchuai <gialongchuai@gmail.com>
2025-04-29 10:53:11 +0700

    galaxy: bai toan tinh dong du cua day so

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,41 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void tsnt_cach01(int n) {
-	int i = 2;
-	while(i*i<=n) {
-		while(n%i==0) {
-			cout << i << " ";
-			n/=i;
-		}
-		i++;
-	}
-	if(n!=1) {
-		cout << n;
-	}
-}
-
-void tsnt_cach02(int n) {
-	for(int i = 2; i*i <= n; i++) {
-		while(n%i==0) {
-			cout << i << " ";
-			n/=i;
-		}
-	}
-	if(n!=1) {
-		cout << n;
-	}
-}
-
 int main() {
-	// phan tan tich thua so nguyen to : 60 = 2 2 3 5
-	int n; cin >> n;
-	
-	tsnt_cach01(n);
-	
-	cout << endl;
-	// bp
-	tsnt_cach02(n);
-	
+	int n; cin >> n; // bai toan dong du (a+b) % c = (a%c+b%c) %c
+	int a = 1e9 + 7;
+	long long c;
+	long long sum = 0;
+	for(int i = 0; i<n; i ++) {
+		cin >> c;
+		sum+= c%a;
+	}
+	cout << sum%a;
 	return 0;
 }
+
+
+
+
+
+
+
+
+
