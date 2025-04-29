8a0acebb663b7c94a9a7946114fd7d137bef6355
gialongchuai <gialongchuai@gmail.com>
2025-04-29 13:45:23 +0700

    galaxy: so luong chan le, tong chan le

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,18 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int n; cin >> n; // bai toan dong du (a+b) % c = (a%c+b%c) %c
-					// bp : cout << ((a%c) % c + (b%c)) % c ...
-	int a = 1e9 + 7;
-	long long c;
-	long long sum = 0;
-	for(int i = 0; i<n; i ++) {
-		cin >> c;
-		sum+= c%a;
-		sum%=a;
+int main() { // so phan tu chan, le; tong phan tu chan, le
+			//5
+			// 301 201 302 18 9
+			// => {2 320
+			// 	   3 511}
+	int n; cin >> n;
+	int a[n];
+	for(int i = 0; i<n; i++) {
+		cin >> a[i];
 	}
-	cout << sum;
+	
+	int dem_l = 0, dem_c = 0;
+	int tong_l = 0, tong_c = 0;
+	
+	for(int i = 0; i<n; i++) {
+		if(a[i]%2==0){
+			tong_c+= a[i];
+			++dem_c;
+		} else {
+			tong_l+= a[i];
+			++dem_l;
+		}
+	}
+	
+	cout << dem_c << " " << tong_c << endl;
+	cout << dem_l << " " << tong_l << endl;
+	
 	return 0;
 }
 
