8f060962b3820152e27548d84d7d426cec474e84
gialongchuai <gialongchuai@gmail.com>
2025-05-02 11:45:05 +0700

    galaxy: pair basic

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,41 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool prime(int n) {
-	if (n<2) return false;
-	for(int i = 2; i*i <= n; i++) {
-		if(n%i==0) return false;
-	}
-	return true;
-}
-
-vector<int> prime_list(vector<int> v) {
-	vector<int> res;
-	for(int x:v) {
-		if(prime(x)) res.push_back(x);
-	}
-	return res;
-}
-
-void in(vector<int> v) {
-	for(int x : v) {
-		cout << x << " ";
-	}
-}
-
-void nhap(vector<int> &v) {
+int main() { // pair basic
 	int n; cin >> n;
-	for(int i = 0; i<n; i++) {
-		int c; cin >> c;
-		v.push_back(c);
+	pair<int,int> p3[n];
+	for(int i=0; i<n; i++) {
+		cin >> p3[i].first >> p3[i].second;
 	}
-}
-
-int main() { // vector basic
-	vector<int> v;
-	nhap(v);
-	vector<int> res = prime_list(v);
-	in(res);
+	cout << "=====" << endl;
+	for(int i=0; i<3; i++) {
+		cout << p3[i].first << " " << p3[i].second << endl;
+	}
+	
+	pair<int,int> p = {3,5};
+	cout << p.first << " " << p.second << endl; // 3 5
+	
+	pair<int,int> p1 = make_pair(10,8);
+	cout << p1.first << " " << p1.second << endl; // 10 8
 }
 
 
