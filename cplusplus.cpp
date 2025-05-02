a5a3f598edb873e698ce642953bf7bdcde974995
gialongchuai <gialongchuai@gmail.com>
2025-05-02 13:45:39 +0700

    galaxy: set voi ham count truy van phan tu

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { //  duyet set
-	set<int> s = {3,2,1,4,4,5,7};
-	for(int x : s) {
-		cout << x << " "; // 1 2 3 4 5 7
+int main() { // truy van phan tu
+	set<int> s;
+	int n, x; cin >> n;
+	for(int i =0; i<n; i++) {
+		cin >> x;
+		s.insert(x);
 	}
-	cout << endl;
-	
-	set<int>::iterator it;
-	for(it = s.begin(); it!=s.end(); it++) {
-		cout << *it << " "; // 1 2 3 4 5 7
+	int t; cin >> t;
+	while(t--) {
+		int x; cin >> x;
+		if(s.count(x)) {
+			cout << "YES" << endl;
+		} else {
+			cout << "NO" << endl;
+		}
 	}
-	cout << endl;
-	
-	set<int>::reverse_iterator it1;
-	for(it1 = s.rbegin(); it1!=s.rend(); it1++) {
-		cout << *it1 << " "; // 7 5 4 3 2 1
-	}
-	cout << endl;
-	
-	cout << *(s.begin()) << " " << *(s.rbegin()) << endl; // min max set
-	cout << *s.begin() << " " << *s.rbegin(); // min max set
 	
 	return 0;
 }
