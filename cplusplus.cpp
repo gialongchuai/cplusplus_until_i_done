35179600a268abc4519907947bfc3f20cb4aa54c
gialongchuai <gialongchuai@gmail.com>
2025-05-02 13:14:30 +0700

    galaxy: set duyet xuoi nguoc va phan tu min max

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,43 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt[100000001];
-
-int main() { // count phan tu khac nhau _ 3 cach
-	// cach 1: 2 vong for
-	int n; cin >> n;
-	int max_val = 0; // max_val cho mang danh dau
-	set<int> s;
-	
-	int a[n], cnt1 = 0;
-	for(int i = 0; i<n; i++) {
-		int tmp; cin >> tmp;
-		a[i] = tmp; // 2 vong for
-		cnt[tmp] = 1; // mang danh dau
-		s.insert(tmp); // set
-		if(a[i] > max_val) max_val = a[i];
+int main() { //  duyet set
+	set<int> s = {3,2,1,4,4,5,7};
+	for(int x : s) {
+		cout << x << " "; // 1 2 3 4 5 7
 	}
+	cout << endl;
 	
-	for(int i = 0; i<n; i++) {
-		bool flag = true;
-		for(int j = i-1; j>=0; j--) {
-			if(a[i] == a[j]) flag = false;
-		}
-		if(flag) cnt1++;
+	set<int>::iterator it;
+	for(it = s.begin(); it!=s.end(); it++) {
+		cout << *it << " "; // 1 2 3 4 5 7
 	}
-	cout << cnt1 << endl;
+	cout << endl;
 	
-	// cach 2: mang danh dau
-	int cnt2 = 0;
-	for(int i = 0; i<=max_val; i++) {
-		if(cnt[i] != 0) {
-			cnt2++;
-		}
+	set<int>::reverse_iterator it1;
+	for(it1 = s.rbegin(); it1!=s.rend(); it1++) {
+		cout << *it1 << " "; // 7 5 4 3 2 1
 	}
-	cout << cnt2 << endl;
+	cout << endl;
 	
-	// cach 3: set
-	cout << s.size();
+	cout << *(s.begin()) << " " << *(s.rbegin()) << endl; // min max set
+	cout << *s.begin() << " " << *s.rbegin(); // min max set
+	
 	return 0;
 }
 
