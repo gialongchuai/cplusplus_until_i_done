bbcc8b0710635309489d87d32f042643283bd6c0
gialongchuai <gialongchuai@gmail.com>
2025-05-02 17:40:27 +0700

    galaxy: map cout element co value chan theo thu tu tang giam key

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // xuat cac so khac nhau cung tang so xuat hien trong day
+int main() { // cout value map chan theo thu tu tang giam key
 	int n; cin >> n;
-	map<int,int> m, m1;
-	int a[n];
+	map<int,int> m;
 	for(int i = 0; i<n; i++) {
-		cin >> a[i];
-		m[a[i]]++;
+		int x; cin >> x;
+		m[x]++;
 	}
 	for(pair<int,int> x : m) {
-		cout << x.first << " " << x.second << endl;
+		if(x.second%2==0) {
+			cout << x.first << " " << x.second << endl;
+		}
 	}
-	cout << "=======" << endl;
-	for(int x : a) {
-		if(m[x]!=0) {
-			cout << x << " " << m[x] << endl;
-			m[x] = 0;
+	
+	cout << "===========" << endl;
+	map<int,int>::reverse_iterator it;
+	for(it = m.rbegin(); it!=m.rend(); it++) {
+		if((*it).second%2==0) {
+			cout << (*it).first << " " << (*it).second << endl;
 		}
 	}
 	
