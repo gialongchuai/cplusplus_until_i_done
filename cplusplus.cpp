7db9e0113525fbab02cac3814d5cdb4fcd12e472
gialongchuai <gialongchuai@gmail.com>
2025-05-05 17:31:25 +0700

    galaxy: bp tong cong don 2 cach

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,19 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long giai_thua[1000000];
-long long mod = 1e9+7;
 
-int main() { // giai thua chia du 
-	int n, x; cin >> n;
-	giai_thua[0] = 1;
-	for(long long i = 1; i<=1000000; i++) {
-		giai_thua[i] = i * giai_thua[i-1];
-		giai_thua[i] %= mod;
+
+int main() { // tong cong don truy cap l, r 
+	int n; cin >> n;
+	int a[n];
+	int b[n];
+	
+	for(int i = 0; i<n; i++) {
+		cin >> a[i];
 	}
-	while(n--) {
-		cin >> x;
-		cout << giai_thua[x] << endl;
+	
+	b[0] = a[0];
+	for(int i = 1; i<n; i++) {
+		b[i] = a[i] + b[i-1];
+	}
+	int x; cin >> x;
+	int l, r;
+	while(x--){
+		cin >> l >> r;
+		l--, r--; // follow 2 cach thuan` va khong thuan` :)
+//		int sum = 0; // cach 01
+//		for(int i = l; i <= r; i++) {
+//			sum+=a[i];
+//		}
+//		cout << sum;
+		if(l==0) cout << b[r];  // cach 02
+		else 
+			cout << b[r] - b[l-1];
 	}
 	
 	return 0;
