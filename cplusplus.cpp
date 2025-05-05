e22a3d947cf840196590730a6621e7939266b47f
gialongchuai <gialongchuai@gmail.com>
2025-05-05 19:53:59 +0700

    galaxy: sort de dem so phan tu khac nhau trong day

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -3,49 +3,18 @@ using namespace std;
 
 
 
-int main() { // sap xep mang tang dan theo 3 cach
+int main() { // dem so phan tu khac nhau trong mang : sort 
 	int n, x; cin >> n;
 	int a[n];
-	map<int,int> m;
-	for(int i = 0; i <n;i++) {
-		cin >>a[i];
-		m[a[i]]++;
+	for(int i = 0; i <n; i++) {
+		cin >> a[i];
 	}
-	for(pair<int,int> x : m) { // cach 01
-		while(x.second--) {
-			cout << x.first  << " ";
-		}
+	sort(a,a+n);
+	int cnt = 1;
+	for(int i = 0; i<n-1;i++) {
+		if(a[i] != a[i+1]) cnt++;
 	}
-	
-	cout << endl;
-	int min_val = 0, max_val = 0;
-	for(int x:a) {
-		if(x>max_val) max_val = x;
-		if(x<min_val) min_val = x;
-	}
-	
-	for(int i = min_val; i<=max_val; i++) { // duyet min max vuot pickelball
-		for(int j = 0; j < n; j++) {
-			if(a[j] == i) {
-				cout << i << " ";
-			}
-		}
-	}
-	cout << endl;
-	for(int i = 0; i<n; i++) { // doi cho voi thang nho nhat tu` i+1 ve sau
-		int min_value = a[i], res = i;
-		for(int j = i+1; j<n; j++) {
-			if(min_value > a[j]) min_value = a[j], res = j;
-		}
-		if(a[i] > min_value) {
-			int tmp = a[i];
-			a[i] = a[res];
-			a[res] = tmp;
-		}
-	}
-	for(int x : a) {
-		cout << x << " ";
-	}
+	cout << cnt;
 	
 	return 0;
 }
