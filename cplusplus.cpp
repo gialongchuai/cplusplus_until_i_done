36d6d5923c3c21d9b155c80fa6531304c9791d2c
gialongchuai <gialongchuai@gmail.com>
2025-05-06 20:52:28 +0700

    galaxy: binary search vi tri phan x xuat hien dau tien va cuoi cung

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,41 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() { // tim phan tu x xuat hien o vi tri dau tien, cuoi cung 
+			// : first_pos ; last_pos
 	int n; cin >> n;
 	int a[n];
-	set<int> s;
-	map<int,int> m;
 	for(int i = 0; i < n; i++) {
 		cin >> a[i];
-		s.insert(a[i]);
-		m[a[i]] = 100;
 	}
-	sort(a, a+n);
-
+	sort(a,a+n);
 	int x; cin >> x;
-	set<int>::iterator it = s.find(x); // truy van theo set
-	if(it!=s.end()) {
-		cout << "YES" << endl;
-	}else {
-		cout << "NO" << endl;
-	}
-	
-	map<int,int>::iterator it_01 = m.find(x); // truy van theo map
-	if(it_01!=m.end()) {
-		cout << "YES" << endl;
-	}else {
-		cout << "NO" << endl;
-	}
-	
-	int t, e; cin >> t; // truy van theo binary_search
-	while(t--) {
-		cin >> e;
-		if(binary_search(a,a+n,e)) {
-			cout << "YES" << endl;
+	int res = 0;
+	int l = 0, r = n - 1;
+	bool flag = false;
+	while(l<=r) {
+		int m = (l+r) / 2;
+		if(a[m] == x) {
+			res = m;
+			r = m - 1; // l = m + 1 neu tim vi tri xuat hien cuoi
+			flag = true;
+		} else if(a[m] > x) {
+			r = m - 1;
 		} else {
-			cout << "NO" << endl;
+			l = m + 1;
 		}
+	}
+	if(flag) {
+		cout << res << endl;
+	} else {
+		cout << "NOT FOUND!" << endl;
+	}
+	for(int x : a) {
+		cout << x << " ";
 	}
 	
 	return 0;
