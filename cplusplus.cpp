86290852a9061a75689f9ec9f31a418a621ceb5a
gialongchuai <gialongchuai@gmail.com>
2025-05-06 13:57:08 +0700

    galaxy: stable sort tang dan tong chu so neu bang nhau so nao xuat hien truoc xep truoc

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,35 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt(int n) {
-	if(n==0) return 1; // 1 so 0 tinh' 1 khong vao duoc while 
-	int count = 0;
-	int tmp;
+int tong(int n) {
+	int sum = 0;
 	while(n) {
-		tmp=n%10;
-		if(tmp == 0 || tmp == 6 || tmp == 8) count++;
+		sum+=n%10;
 		n/=10;
 	}
-	return count;
+	return sum;
 }
 
 bool cmp(int x, int y) {
-	int cnt_x = cnt(x);
-	int cnt_y = cnt(y);
-	if(cnt_x != cnt_y) {
-		return cnt_x > cnt_y;
-	}
-	return x < y;
+	return tong(x) < tong(y);
 }
 
-int main() { // sort : dem 0 6 8 xuat hien nhieu nhat
-			// trung in so nho toi lon
+int main() { // stable sort : merge sort (do on dinh con so)
+			// sap xep theo tong chu so tang dan
+			// neu bang nhau giu nguyen vi tri xuat hien == so nao truoc xep truoc
+			// => dung stable sort
 	int n, x; cin >> n;
 	int a[n];
 	for(int i = 0; i <n; i++) {
 		cin >> a[i];
 	}
-	sort(a,a+n,cmp);
+	stable_sort(a,a+n,cmp);
 	for(int x : a) {
 		cout << x << " ";
 	}
