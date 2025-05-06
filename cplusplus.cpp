e745cb58fa91c9be999b6be69113b9f7776138ad
gialongchuai <gialongchuai@gmail.com>
2025-05-06 13:13:43 +0700

    galaxy: sort voi cmp xet theo nhieu tieu chi

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,21 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int tong(int n) {
+	int sum = 0;
+	while(n) {
+		sum+=n%10;
+		n/=10;
+	}
+	return sum;
+}
 
+bool cmp(int x, int y) {
+	int sum_x = tong(x);
+	int sum_y = tong(y);
+	if(sum_x != sum_y) {
+		return sum_x < sum_y;
+	}
+	return x < y;
+}
 
-int main() { // dem so phan tu khac nhau trong mang : sort 
+int main() { // sort : tang dan theo tong cua cac chu so
+			// bang nhau so nho dung truoc
+		//	8
+		//		100001 2 30002 5 303003 212 83 11
+		// ->	2 11 100001 5 212 30002 303003 83
 	int n, x; cin >> n;
 	int a[n];
 	for(int i = 0; i <n; i++) {
 		cin >> a[i];
 	}
-	sort(a,a+n);
-	int min_val = a[1] - a[0];
-	for(int i = 0; i<n-1;i++) {
-		int r = a[i+1] - a[i];
-		if(r<min_val) min_val = r;
+	sort(a,a+n,cmp);
+	for(int x : a) {
+		cout << x << " ";
 	}
-	cout << min_val;
 	
 	return 0;
 }
