05f9503c4e286cd564c788fb6986f2dccbe614af
gialongchuai <gialongchuai@gmail.com>
2025-05-06 17:02:36 +0700

    galaxy: con tro can ban

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,33 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tong(int n) {
-	int sum = 0;
-	while(n) {
-		sum+=n%10;
-		n/=10;
-	}
-	return sum;
-}
-
-bool cmp(int x, int y) {
-	return tong(x) < tong(y);
-}
-
-int main() { // stable sort : merge sort (do on dinh con so)
-			// sap xep theo tong chu so tang dan
-			// neu bang nhau giu nguyen vi tri xuat hien == so nao truoc xep truoc
-			// => dung stable sort
-	int n, x; cin >> n;
-	int a[n];
-	for(int i = 0; i <n; i++) {
-		cin >> a[i];
-	}
-	stable_sort(a,a+n,cmp);
-	for(int x : a) {
-		cout << x << " ";
-	}
-	
+int main() {
+	int n = 100;
+	cout << &n << endl; // 0x73fe1c
+	int *ptr = &n;
+	cout << ptr << endl; // 0x73fe1c
+	cout << &ptr << endl; // 0x73fe10
+	cout << *ptr << endl; // 100
+	int **ptr1 = &ptr;
+	cout << ptr1 << endl; // 0x73fe10
+	cout << &ptr1 << endl; // 0x73fe08
+	cout << *ptr1 << endl; // 0x73fe1c
+	cout << **ptr1 << endl; // 100
 	return 0;
 }
 
