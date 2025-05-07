a00e059aae0511b0dcbad7878174c7e308522fe1
gialongchuai <gialongchuai@gmail.com>
2025-05-07 12:53:38 +0700

    galaxy: binary search phan tu dau tien nho hoac bang

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // tim phan tu x xuat hien o vi tri dau tien, cuoi cung 
-			// : first_pos ; last_pos
+int main() { // 10
+			//1 2 2 2 2 4 5 6 7 110
+			//2
+			//2
+			
+			// 10
+			//1 2 2 2 2 4 5 6 7 110
+			//88
+			//7
 	int n; cin >> n;
 	int a[n];
 	for(int i = 0; i < n; i++) {
@@ -15,18 +22,16 @@ int main() { // tim phan tu x xuat hien o vi tri dau tien, cuoi cung
 	bool flag = false;
 	while(l<=r) {
 		int m = (l+r) / 2;
-		if(a[m] == x) {
+		if(a[m] <= x) {
 			res = m;
-			r = m - 1; // l = m + 1 neu tim vi tri xuat hien cuoi
+			l = m + 1; 
 			flag = true;
-		} else if(a[m] > x) {
-			r = m - 1;
 		} else {
-			l = m + 1;
+			r = m -1;
 		}
 	}
 	if(flag) {
-		cout << res << endl;
+		cout << a[res] << endl;
 	} else {
 		cout << "NOT FOUND!" << endl;
 	}
