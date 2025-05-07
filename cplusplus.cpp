5abcb75dbd5bd5d9ffdf425462865b80e48fde21
gialongchuai <gialongchuai@gmail.com>
2025-05-07 14:37:55 +0700

    galaxy: binary search dem xem moi phan tu cua mang a thi co bao nhieu phan tu nho hon o mang b

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,42 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // 10
-			//1 2 2 2 2 4 5 6 7 110
-			//2
-			//2
-			
-			// 10
-			//1 2 2 2 2 4 5 6 7 110
-			//88
-			//7
-	int n; cin >> n;
-	int a[n];
-	for(int i = 0; i < n; i++) {
+int main() { // cho 2 day: voi moi phan tu day 1 
+			// thi co bao nhieu so nho hon no ben day 2
+			// 5 8
+			// 2 8 7 2 8 			{2:1,0,1} ; {8:3,7,1,0,4,1,5} ...
+			// 9 3 7 1 0 4 1 5
+			// => 3 7 6 3 7
+	int n, m; cin >> n >> m;
+	int a[n], b[m];
+	for(int i = 0; i <n; i++) {
 		cin >> a[i];
 	}
-	sort(a,a+n);
-	int x; cin >> x;
-	int res = 0;
-	int l = 0, r = n - 1;
-	bool flag = false;
-	while(l<=r) {
-		int m = (l+r) / 2;
-		if(a[m] <= x) {
-			res = m;
-			l = m + 1; 
-			flag = true;
-		} else {
-			r = m -1;
-		}
-	}
-	if(flag) {
-		cout << a[res] << endl;
-	} else {
-		cout << "NOT FOUND!" << endl;
+	for(int i = 0; i <m; i++) {
+		cin >> b[i];
 	}
 	for(int x : a) {
+		int cnt = 0;
+		for(int y : b) {
+			if(y<x) cnt++;
+		}
+		cout << cnt << " ";
+	}
+	cout << endl;
+	sort(b,b+m);
+	for(int x : b) {
 		cout << x << " ";
+	}
+	cout << endl;
+
+	for(int x : a){
+		int res = 0;
+		int l = 0, r = m - 1;
+		bool flag = true;
+		while(l<=r) {
+			int m = (l+r) / 2;
+			if(b[m] < x) {
+				l = m + 1;
+				res = m;
+				flag = false;
+			} else {
+				r = m - 1;
+			}
+		}
+	if(flag) cout << res << " ";
+	else cout << res+1 << " ";
 	}
 	
 	return 0;
