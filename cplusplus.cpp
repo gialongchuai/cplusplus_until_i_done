b981088d925163c6ee48d1a799442fedde2abb59
gialongchuai <gialongchuai@gmail.com>
2025-05-07 20:34:41 +0700

    galaxy: mang 2 chieu tim diem cuc dai lon hon 8 phan tu xung quanh no

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // 2 cach hoan vi 2 hang trong ma tran vuong
-			// 5
-			// 1 2 3 4 10
-			// 5 6 7 8 12
-			// 9 10 1 2 3
-			// 2 3 5 7 6
-			// 4 6 2 1 6
-			// 2 3
-			// 1 2 3 4 10
-			// 9 10 1 2 3  	*******
-			// 5 6 7 8 12  	*******
-			// 2 3 5 7 6
-			// 4 6 2 1 6
-	int n; cin >> n;
-	int a[n][n];
+int main() { // tim diem cuc dai co 
+			// 8 phan tu xung quanh nho hon chinh no
+	int n, m; cin >> n >> m;
+	int a[n][m];
 	for(int i = 0; i <n; i++) {
-		for(int j = 0; j < n; j++){
+		for(int j = 0; j < m; j++){
 			cin >> a[i][j];
 		}
 	}
 	
-	int h1, h2; cin >> h1 >> h2;
-	for(int i = h1-1; i < n; i++) {
-		for(int j = 0; j<n; j++) {
-			int tmp = a[i][j];
-			a[i][j] = a[h2-1][j];
-			a[h2-1][j] = tmp;
-		}
-		break;
-	}
-
-	// bp
-	for(int j = 0; j<n; j++) {
-		int tmp = a[h1-1][j];
-		a[h1-1][j] = a[h2-1][j];
-		a[h2-1][j] = tmp;
-	}
-	
-	for(int i = 0; i<n; i++) {
-		for(int j = 0; j <n; j++) {
-			cout << a[i][j] << " ";
+	int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+	int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+	int cnt = 0;
+	for(int i = 0; i <n; i++) {
+		for(int j =0; j<m; j++) {
+			bool flag = true;
+			for(int k = 0; k<8; k++) {
+				int i1 = i + dx[k];
+				int y1 = j + dy[k];
+				if(i1>=0 && i1<n && y1>=0 && y1<m && a[i][j] <= a[i1][y1]) {
+					flag = false;
+				}
+			}
+			if(flag) {
+				cnt++;
+				cout << a[i][j] << " ";
+			}
 		}
 		cout << endl;
 	}
+	cout << cnt;
 	
 	return 0;
 }
