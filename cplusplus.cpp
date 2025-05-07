1f4590f077dc3fd5c576a86cb6874b543a290b1f
gialongchuai <gialongchuai@gmail.com>
2025-05-07 19:34:02 +0700

    galaxy: hoan vi 2 hang trong ma tran

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // duyet theo pattern
+int main() { // 2 cach hoan vi 2 hang trong ma tran vuong
+			// 5
+			// 1 2 3 4 10
+			// 5 6 7 8 12
+			// 9 10 1 2 3
+			// 2 3 5 7 6
+			// 4 6 2 1 6
+			// 2 3
+			// 1 2 3 4 10
+			// 9 10 1 2 3  	*******
+			// 5 6 7 8 12  	*******
+			// 2 3 5 7 6
+			// 4 6 2 1 6
 	int n; cin >> n;
 	int a[n][n];
 	for(int i = 0; i <n; i++) {
@@ -10,33 +22,25 @@ int main() { // duyet theo pattern
 		}
 	}
 	
-	cout << "Pattern 01:\n";
-	for(int i = 0; i<n; i++) {
+	int h1, h2; cin >> h1 >> h2;
+	for(int i = h1-1; i < n; i++) {
 		for(int j = 0; j<n; j++) {
-			cout << a[j][i] << " ";
+			int tmp = a[i][j];
+			a[i][j] = a[h2-1][j];
+			a[h2-1][j] = tmp;
 		}
-		cout << endl;
+		break;
+	}
+
+	// bp
+	for(int j = 0; j<n; j++) {
+		int tmp = a[h1-1][j];
+		a[h1-1][j] = a[h2-1][j];
+		a[h2-1][j] = tmp;
 	}
 	
-	cout << "Pattern 02:\n";
-	for(int i = n-1; i>=0; i--) {
-		for(int j = n-1; j>=0; j--) {
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
-	}
-	
-	cout << "Pattern 03:\n";
-	for(int i = n-1; i>=0; i--) {
-		for(int j = 0; j<n; j++) {
-			cout << a[j][i] << " ";
-		}
-		cout << endl;
-	}
-	
-	cout << "Pattern 04:\n";
 	for(int i = 0; i<n; i++) {
-		for(int j = n-1; j>=0; j--) {
+		for(int j = 0; j <n; j++) {
 			cout << a[i][j] << " ";
 		}
 		cout << endl;
