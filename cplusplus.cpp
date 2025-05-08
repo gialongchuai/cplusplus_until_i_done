30eaca85081559862649eed3f9f313fbebe29da9
gialongchuai <gialongchuai@gmail.com>
2025-05-08 10:02:08 +0700

    galaxy: dem so quan dao trong ma tran bang thuat toan dfs duyet lan 4 o xung quanh

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,37 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // tim diem cuc dai co 
-			// 8 phan tu xung quanh nho hon chinh no
+int dx[4] = {-1, 0, 0, 1};
+int dy[4] = {0, -1, 1, 0};
+int a[100][100];
+
+void dfs(int i, int j, int n, int m) {
+	a[i][j] = 0;
+	for(int k=0; k<8; k++) {
+		int i1 = i + dx[k];
+		int y1 = j + dy[k];
+		if(i1>=0 && i1 <n && y1>=0 && y1<m && a[i1][y1] == 1) {
+			dfs(i1,y1,n,m);
+		}
+	}
+}
+
+int main() { // count island
+			// dem xem co bao nhieu mien`, tuc la 4 o xung quanh co bang no khong
+			// 4 5
+			// 1 1 0 0 0
+			// 1 1 0 0 1
+			// 1 0 0 1 1
+			// 0 1 0 1 1
+			// 3 mien` duoc danh dau so 1 rieng biet : dfs
 	int n, m; cin >> n >> m;
-	int a[n][m];
-	for(int i = 0; i <n; i++) {
-		for(int j = 0; j < m; j++){
+	for(int i =0; i<n; i++) {
+		for(int j = 0; j<m; j++) {
 			cin >> a[i][j];
 		}
 	}
 	
-	int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-	int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+	
 	int cnt = 0;
 	for(int i = 0; i <n; i++) {
-		for(int j =0; j<m; j++) {
-			bool flag = true;
-			for(int k = 0; k<8; k++) {
-				int i1 = i + dx[k];
-				int y1 = j + dy[k];
-				if(i1>=0 && i1<n && y1>=0 && y1<m && a[i][j] <= a[i1][y1]) {
-					flag = false;
-				}
-			}
-			if(flag) {
+		for(int j = 0; j<m; j++) {
+			if(a[i][j] == 1) {
 				cnt++;
-				cout << a[i][j] << " ";
+				dfs(i,j,n,m);
 			}
 		}
-		cout << endl;
 	}
+	
 	cout << cnt;
+	
 	
 	return 0;
 }
