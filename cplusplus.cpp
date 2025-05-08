034a75987bf4f19d5fa7b448ff2a5d6d60394827
gialongchuai <gialongchuai@gmail.com>
2025-05-08 12:16:04 +0700

    galaxy: dfs tim duong di cua con chuot hop le

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -3,47 +3,64 @@ using namespace std;
 
 int dx[4] = {-1, 0, 0, 1};
 int dy[4] = {0, -1, 1, 0};
-int a[100][100];
+int a[500][500];
 
-void dfs(int i, int j, int n, int m) {
+void dfs(int i, int j, int z, int t, int n, int m, bool &flag) {
 	a[i][j] = 0;
 	for(int k=0; k<4; k++) {
 		int i1 = i + dx[k];
 		int y1 = j + dy[k];
 		if(i1>=0 && i1 <n && y1>=0 && y1<m && a[i1][y1] == 1) {
-			dfs(i1,y1,n,m);
+			if(i1==z && y1==t) {
+				flag = true;
+				return;
+			}
+			dfs(i1,y1,z,t,n,m,flag);
 		}
 	}
 }
 
-int main() { // count island
-			// dem xem co bao nhieu mien`, tuc la 4 o xung quanh co bang no khong
-			// 4 5
-			// 1 1 0 0 0
-			// 1 1 0 0 1
-			// 1 0 0 1 1
-			// 0 1 0 1 1
-			// 3 mien` duoc danh dau so 1 rieng biet : dfs
+int main() { // tim duong di cua con chuot thong qua 4 o xung quanh
+//		3 5
+//		1 1 3 5
+//		1 0 1 0 0
+//		0 1 1 1 1
+//		1 0 0 0 1
+// => 0;0 => 2;4 => NO
 	int n, m; cin >> n >> m;
-	for(int i =0; i<n; i++) {
+	int b[4];
+	for(int i = 0; i<4; i++) {
+		cin >> b[i];
+	}
+	
+	for(int i=0; i<n; i++) {
 		for(int j = 0; j<m; j++) {
 			cin >> a[i][j];
 		}
 	}
 	
+	int i = b[0] - 1;
+	int j = b[1] - 1;
 	
-	int cnt = 0;
-	for(int i = 0; i <n; i++) {
-		for(int j = 0; j<m; j++) {
-			if(a[i][j] == 1) {
-				cnt++;
-				dfs(i,j,n,m);
-			}
-		}
+	int z = b[2] - 1;
+	int t = b[3] - 1;
+	bool flag = false;
+	dfs(i,j,z,t,n,m,flag);
+	
+	if(flag) {
+		cout << "yes";
+	} else {
+		cout << "no";
 	}
-	
-	cout << cnt;
-	
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
