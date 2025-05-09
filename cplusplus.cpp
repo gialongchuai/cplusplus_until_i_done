26ba3f09b6ec575c76f28acbade9568a1ace2933
gialongchuai <gialongchuai@gmail.com>
2025-05-09 18:11:20 +0700

    galaxy: stringstream chuan hoa ten

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,21 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // cout tu cung tan xuat theo thu tu xuat hien
-	string s;
-	map<string,int> m;
+string chuan_hoa(string s) {
+	string result = "";
 	vector<string> v;
-	while(cin>>s) {
-		m[s]++;
-		v.push_back(s);
-	}
 	
-	for(string x : v) {
-		if(m[x] != 0) {
-			cout << x << " " << m[x] << endl;
-			m[x] = 0;
-		}
+	stringstream ss(s);
+	string word;
+	while(ss >> word) {
+		v.push_back(word);
 	}
+	for(string &x:v) {
+		for(char &z : x) {
+			z = tolower(z);
+		}
+		x[0] = toupper(x[0]);
+	}
+	for(string x : v) {
+		result += x + ' ';
+	}
+	return result;
+}
+
+string bp_chuan_hoa(string s) {
+	string name;
+	stringstream ss(s);
+	string word;
+	while(ss >> word) {
+		name += toupper(word[0]);
+		for(int i = 1 ; i < word.size(); i++) {
+			name += tolower(word[i]);
+		}
+		name += ' ';
+	}
+	name.pop_back(); // loai bo ki tu space cuoi'
+	return name;
+}
+
+int main() { // chuan hoa ten: nGuYen VAN   naM
+			// => Nguyen Van Nam
+	string s;
+	getline(cin,s);
+	cout << chuan_hoa(s) << endl;
+	cout << bp_chuan_hoa(s);
 	
 	return 0;
 }
