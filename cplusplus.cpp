fc8c868959d6530acfb2c8740e47db2f22b57879
gialongchuai <gialongchuai@gmail.com>
2025-05-09 19:27:28 +0700

    galaxy: bp stringstream chuan hoa ten theo 2 dieu kien

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> chuan_hoa_ten(string s) {
+string chuanHoa(string s) {
 	string name = "";
-	stringstream ss(s);
-	string word;
-	vector<string> v;
-	while(ss >> word) {
-		word[0] = toupper(word[0]);
-		for(int i =1; i<word.size() ;i++) {
-			word[i] = tolower(word[i]);
-		}
-		v.push_back(word);
+	name += toupper(s[0]);
+	for(int i = 1; i < s.size(); i++) {
+		name += tolower(s[i]);
 	}
-	for(char &x : v[v.size()-1]) {
+	return name;
+}
+
+string inHoa(string s) {
+	for(char &x : s) {
 		x = toupper(x);
 	}
-	return v;
+	return s;
 }
 
 int main() { // chuan hoa ten:
@@ -24,24 +22,30 @@ int main() { // chuan hoa ten:
 				//
 				//Tran Van, TECH28
 				//TECH28, Tran Van
-				
+	string s;
+	vector<string> v;
 	string name;
-	getline(cin,name);
-	vector<string> v = chuan_hoa_ten(name);
-	string s1 = "";
-	for(int i = 0 ; i<v.size()-1;i++) {
-		s1 += v[i] + ' ';
+	while(cin >> s) {
+		name = chuanHoa(s);
+		v.push_back(name);
 	}
-	s1.pop_back();
-	cout << s1 << ", " << v[v.size()-1] << endl;
-	
-	cout << v[v.size()- 1] << ", ";
-	string s2 = "";
-	for(int i = 0 ; i<v.size()-1;i++) {
-		s2 += v[i] + ' ';
+	v[v.size()-1] = inHoa(v[v.size()-1]);
+	for(int i = 0; i <v.size() -1; i++) {
+		if(i!=v.size()-2) {
+			cout << v[i] << " ";
+		} else {
+			cout << v[i] << ", ";
+		}
 	}
-	s2.pop_back();
-	cout << s2;
+	cout << v[v.size()-1] << endl;
+	cout << v[v.size()-1] << ", ";
+		for(int i = 0; i <v.size() -1; i++) {
+		if(i!=v.size()-2) {
+			cout << v[i] << " ";
+		} else {
+			cout << v[i];
+		}
+	}
 	
 	return 0;
 }
