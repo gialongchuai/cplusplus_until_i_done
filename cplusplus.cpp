f3ab2c0d4722daaeb5327ec918ea0c4d1681fea2
gialongchuai <gialongchuai@gmail.com>
2025-05-09 20:17:56 +0700

    galaxy: chuan hoa email va mat khau

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,50 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string chuanHoa(string s) {
-	string name = "";
-	name += toupper(s[0]);
-	for(int i = 1; i < s.size(); i++) {
-		name += tolower(s[i]);
-	}
-	return name;
-}
-
-string inHoa(string s) {
-	for(char &x : s) {
-		x = toupper(x);
-	}
-	return s;
-}
-
-int main() { // chuan hoa ten:
-				//tRAN    van  tECH28
-				//
-				//Tran Van, TECH28
-				//TECH28, Tran Van
-	string s;
-	vector<string> v;
+vector<string> chuan_hoa_space(string s) {
+	stringstream ss(s);
 	string name;
-	while(cin >> s) {
-		name = chuanHoa(s);
-		v.push_back(name);
+	vector<string> v;
+	string word;
+	while(ss >> word) {
+		v.push_back(word);
 	}
-	v[v.size()-1] = inHoa(v[v.size()-1]);
-	for(int i = 0; i <v.size() -1; i++) {
-		if(i!=v.size()-2) {
-			cout << v[i] << " ";
-		} else {
-			cout << v[i] << ", ";
-		}
+	return v;
+}
+
+vector<string> chuan_hoa_dau_gach(string s) {
+	stringstream ss(s);
+	string name;
+	vector<string> v;
+	string word;
+	while(getline(ss,word,'/')) {
+		v.push_back(word);
 	}
-	cout << v[v.size()-1] << endl;
-	cout << v[v.size()-1] << ", ";
-		for(int i = 0; i <v.size() -1; i++) {
-		if(i!=v.size()-2) {
-			cout << v[i] << " ";
-		} else {
-			cout << v[i];
+	return v;
+}
+
+int main() { // chuan hoa email: 
+				//2
+				//nguyen  VAN  NAM    22/07/2002
+				//le THI  HONG nHUNG 30/07/1991
+	int n; cin >> n; cin.ignore();
+	while(n--) {
+		string s;
+		getline(cin,s);
+		for(char &x : s) {
+			x = tolower(x);
 		}
+		vector<string> v = chuan_hoa_space(s);
+		cout << v[v.size()-2]; 
+		for(int i = 0; i<v.size()-2; i++) {
+			string x = v[i];
+			cout << x[0];
+		}
+		cout << "@xyz.edu.vn" << endl;
+		vector<string> birth = chuan_hoa_dau_gach(v[v.size()-1]);
+		for(string x : birth) {
+			if(x[0] == '0') x.erase(0,1);
+			cout << x;
+		}
+		cout << endl;
 	}
 	
 	return 0;
