2f62787aafed9b515e60e83b1733ee43f0800a2b
gialongchuai <gialongchuai@gmail.com>
2025-05-09 14:38:40 +0700

    galaxy: string char cung tan suat xuat hien

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // cout string cung tan xuat 
+int main() { // cout ki tu cung tan xuat 
 			// 28tech dev abc 28tech dev c dev abc def
 			//^Z
-			//28tech 2
-			//abc 2
-			//c 1
-			//def 1
-			//dev 3
-	map<string,int> m;
+			//2 2
+			//8 2
+			//a 2
+			//b 2
+			//c 5
+			//d 4
+			//e 6
+			//f 1
+			//h 2
+			//t 2
+			//v 3
+	map<char,int> m;
 	string s;
  	while(cin >> s) {
-		m[s]++;
+		for(char x : s) {
+			m[x]++;
+		}
 	}
-	for(pair<string,int> x : m) {
+	for(pair<char,int> x : m) {
 		cout << x.first << " " << x.second << endl;
 	}
 
