8e3963ed8d9965c8dfc339754c56aebf494a5ec8
gialongchuai <gialongchuai@gmail.com>
2025-05-09 15:41:49 +0700

    galaxy: string dem tu in hoa

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,30 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // cout ki tu cung tan xuat 
-			// 28tech dev abc 28tech dev c dev abc def
-			//^Z
-			//2 2
-			//8 2
-			//a 2
-			//b 2
-			//c 5
-			//d 4
-			//e 6
-			//f 1
-			//h 2
-			//t 2
-			//v 3
-	map<char,int> m;
+bool check_upper(string s) {
+	for(char x : s) {
+		if(x<65||x>90) return false; // if(!isupper(x)) return false;
+	}
+	return true;
+}
+
+int main() { // dem cac tu tat ca in hoa
 	string s;
- 	while(cin >> s) {
-		for(char x : s) {
-			m[x]++;
-		}
+	int cnt = 0;
+	while(cin >> s) {
+		if(check_upper(s)) cnt++;
 	}
-	for(pair<char,int> x : m) {
-		cout << x.first << " " << x.second << endl;
-	}
+	cout << cnt;
 
 	return 0;
 }
