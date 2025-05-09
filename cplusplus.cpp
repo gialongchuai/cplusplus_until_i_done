30cefd39764d07056ddbf43ac7ee09c6f5cbce79
gialongchuai <gialongchuai@gmail.com>
2025-05-09 20:29:46 +0700

    galaxy: maybe bp chuan hoa email va mat khau

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,28 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> chuan_hoa_space(string s) {
-	stringstream ss(s);
-	string name;
-	vector<string> v;
-	string word;
-	while(ss >> word) {
-		v.push_back(word);
-	}
-	return v;
-}
-
-vector<string> chuan_hoa_dau_gach(string s) {
-	stringstream ss(s);
-	string name;
-	vector<string> v;
-	string word;
-	while(getline(ss,word,'/')) {
-		v.push_back(word);
-	}
-	return v;
-}
-
 int main() { // chuan hoa email va mat khau
 			//2
 			//nguyen  VAN  NAM    02/07/2002
@@ -38,18 +16,26 @@ int main() { // chuan hoa email va mat khau
 		for(char &x : s) {
 			x = tolower(x);
 		}
-		vector<string> v = chuan_hoa_space(s);
-		cout << v[v.size()-2]; 
-		for(int i = 0; i<v.size()-2; i++) {
-			string x = v[i];
-			cout << x[0];
+		stringstream ss(s);
+		string word;
+		vector<string> v;
+		while(ss >> word) {
+			v.push_back(word);		
 		}
-		cout << "@xyz.edu.vn" << endl;
-		vector<string> birth = chuan_hoa_dau_gach(v[v.size()-1]);
-		for(string x : birth) {
-			if(x[0] == '0') x.erase(0,1);
+		int n = v.size();
+		string email = v[n-2];
+		for(int i = 0; i < n-2; i++) {
+			email += v[i][0]; // lay chu cai dau tien trong tung ki tu
+		}
+		cout << email << "@xyz.edu.vn" << endl; 
+		string ns = v[n-1];
+		stringstream zz(ns);
+		string token;
+		while(getline(zz,token,'/')) {
+			int x = stoi(token);
 			cout << x;
 		}
+		
 		cout << endl;
 	}
 	
