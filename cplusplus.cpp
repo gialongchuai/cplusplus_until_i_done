29765a92f8fd47edca36dcf9f82402a81f513890
gialongchuai <gialongchuai@gmail.com>
2025-05-09 18:44:02 +0700

    galaxy: stringstream chuan hoa ten ngay sinh

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,49 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string chuan_hoa(string s) {
-	string result = "";
-	vector<string> v;
+string chuan_hoa_ten(string name) {
+	string name_result = "";
+	stringstream ss(name);
+	string word;
+	while(ss >> word) {
+		name_result += toupper(word[0]);
+		for(int i =1; i<word.size() ;i++) {
+			name_result += tolower(word[i]);
+		}
+		name_result += ' ';
+	}
+	name_result.pop_back(); // loai bo space cuoi'
+	return name_result;
+}
+
+string chuan_hoa_ngay_sinh(string birth) {
+	string birth_result = "";
+	stringstream ss(birth);
+	string word;
+	while(getline(ss, word, '/')) {
+		if(word.size() == 1) word.insert(0, "0");
+		birth_result += word + '/';
+	}
 	
-	stringstream ss(s);
-	string word;
-	while(ss >> word) {
-		v.push_back(word);
-	}
-	for(string &x:v) {
-		for(char &z : x) {
-			z = tolower(z);
-		}
-		x[0] = toupper(x[0]);
-	}
-	for(string x : v) {
-		result += x + ' ';
-	}
-	result.pop_back();
-	return result;
+	birth_result.pop_back(); // loai bo / cuoi'
+	return birth_result;
 }
 
-string bp_chuan_hoa(string s) {
-	string name = "";
-	stringstream ss(s);
-	string word;
-	while(ss >> word) {
-		name += toupper(word[0]);
-		for(int i = 1 ; i < word.size(); i++) {
-			name += tolower(word[i]);
-		}
-		name += ' ';
-	}
-	name.pop_back(); // loai bo ki tu space cuoi'
-	return name;
-}
-
-int main() { // chuan hoa ten: nGuYen VAN   naM
-			// => Nguyen Van Nam
-	string s;
-	getline(cin,s);
-	cout << chuan_hoa(s) << endl;
-	cout << bp_chuan_hoa(s);
+int main() { // chuan hoa sinh vien: NgUYEN   Van  TECH28
+									//1/1/2022
+								// Nguyen Van Tech28
+								// 01/01/2022	
+	string name, birth;
+	getline(cin, name);
+	getline(cin, birth);
+	
+	string bp_birth = birth;
+	
+	string name_result = chuan_hoa_ten(name);
+	string birth_result = chuan_hoa_ngay_sinh(birth);
+	cout << name_result << endl << birth_result;
+	
+	if(bp_birth[1] == '/') bp_birth = "0" + bp_birth;
+	if(bp_birth[4] == '/') bp_birth.insert(3,"0");
+	cout << endl << bp_birth;
 	
 	return 0;
 }
