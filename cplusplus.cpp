20cf20b85fe27a2b2aff5d8bd4d545777d62719b
gialongchuai <gialongchuai@gmail.com>
2025-05-10 10:28:17 +0700

    galaxy: chuan hoa email va mat khau tranh trung email

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,18 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // chuan hoa email va mat khau
-			//2
-			//nguyen  VAN  NAM    02/07/2002
-			//namnv@xyz.edu.vn
-			//272002
-			//le THI  HONG nHUNG 8/02/1991
-			//nhunglth@xyz.edu.vn
-			//821991
-	int n; cin >> n; cin.ignore();
-	while(n--) {
-		string s;
-		getline(cin,s);
+int main() { // chuan hoa email, pass tranh trung email
+	int n; cin >>n; cin.ignore();
+	string s;
+	map<string,int> map_email;
+	while(getline(cin,s)) {
 		for(char &x : s) {
 			x = tolower(x);
 		}
@@ -20,20 +13,27 @@ int main() { // chuan hoa email va mat khau
 		string word;
 		vector<string> v;
 		while(ss >> word) {
-			v.push_back(word);		
+			v.push_back(word);
 		}
 		int n = v.size();
 		string email = v[n-2];
-		for(int i = 0; i < n-2; i++) {
-			email += v[i][0]; // lay chu cai dau tien trong tung ki tu
+		for(int i = 0; i < n -2; i++) {
+			email += v[i][0];
 		}
-		cout << email << "@xyz.edu.vn" << endl; 
+
+		if(!map_email.count(email)) {
+			cout << email << "@xyz.edu.vn" << endl;
+			map_email[email]++;
+		} else {
+			cout << email << map_email[email]+1 << "@xyz.edu.vn" << endl;
+			map_email[email]++;
+		}
 		string ns = v[n-1];
 		stringstream zz(ns);
 		string token;
-		while(getline(zz,token,'/')) {
-			int x = stoi(token);
-			cout << x;
+		while(getline(zz,token, '/')) {
+			if(token[0] == '0') token.erase(0,1);
+			cout << token;
 		}
 		
 		cout << endl;
