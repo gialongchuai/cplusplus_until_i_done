b621151ac6730252ae47c15a1f8d466bf9eb0dbd
gialongchuai <gialongchuai@gmail.com>
2025-05-10 19:35:19 +0700

    galaxy: string cout so lon nhat co m chu so va tong cac chu so bang x

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -8,15 +8,52 @@ bool cmp(string a, string b){
 }
 
 int main() {
-		// tinh tong cac chu so trong so cuc lon
-		// 81212112312312312331823239
-		// => 70
-	string s; cin >> s;
-	int sum = 0;
-	for(char x : s) {
-		sum+=x-'0';
+		// cout so lon nhat co m chu so ma tong bang` s
+		//	4 23
+		// => 9950
+		// 5 0
+		// => NOT FOUND
+	int m, s; cin >> m >> s;
+	if(m==0) {
+		cout << "NOT FOUND";
+		return 0;
 	}
-	cout << sum;
+	map<int,int> ma;
+	int x = 9;
+	while(s) {
+		while(s-x>=0) {
+			ma[x]++;
+			s-=x;
+		}
+		x--;
+	}
+	
+	map<int,int>::reverse_iterator it;
+	int cnt = 0;
+	string result = "";
+	for(it = ma.rbegin(); it != ma.rend(); it++) {
+		int x = (*it).second;
+		for(int i = 0; i<x ;i++) {
+			cnt++;
+			result+= to_string((*it).first);
+		}
+	}
+	if(cnt > m) {
+		cout << "NOT FOUND";
+		return 0;
+	}
+	
+	int size_so = m - cnt;
+	while(size_so--) {
+		result.push_back('0');
+	}
+	int so = stoi(result); // 00000 => NOT FOUND
+	string so_sanh = to_string(so);
+	if(so_sanh.size() == result.size()) {
+		cout << result;
+	} else {
+		cout << "NOT FOUND";
+	}
 
 	return 0;
 }
