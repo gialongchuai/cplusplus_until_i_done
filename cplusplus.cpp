b51c026540307ff6f740a0c0469d3af5b45dc5a0
gialongchuai <gialongchuai@gmail.com>
2025-05-10 10:42:28 +0700

    galaxy: sort string theo nhieu tieu chi

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,57 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // chuan hoa email, pass tranh trung email
-			//4
-			//nguyen   van LOng 20/10/2002
-			//longnv@xyz.edu.vn
-			//20102002
-			//ngo Vang Long 20/02/2002
-			//longnv2@xyz.edu.vn
-			//2022002
-			//Nguyen   Vu Long 02/12/2003
-			//longnv3@xyz.edu.vn
-			//2122003
-			//ho van   nam 17/09/2002
-			//namhv@xyz.edu.vn
-			//1792002
-	int n; cin >>n; cin.ignore();
-	string s;
-	map<string,int> map_email;
-	while(getline(cin,s)) {
-		for(char &x : s) {
-			x = tolower(x);
-		}
-		stringstream ss(s);
-		string word;
-		vector<string> v;
-		while(ss >> word) {
-			v.push_back(word);
-		}
-		int n = v.size();
-		string email = v[n-2];
-		for(int i = 0; i < n -2; i++) {
-			email += v[i][0];
-		}
-
-		if(!map_email.count(email)) {
-			cout << email << "@xyz.edu.vn" << endl;
-			map_email[email]++;
-		} else {
-			cout << email << map_email[email]+1 << "@xyz.edu.vn" << endl;
-			map_email[email]++;
-		}
-		string ns = v[n-1];
-		stringstream zz(ns);
-		string token;
-		while(getline(zz,token, '/')) {
-			if(token[0] == '0') token.erase(0,1);
-			cout << token;
-		}
-		
-		cout << endl;
+bool cmp(string s1, string s2) {
+	if(s1.size() !=s2.size()) {
+		return s1.size() < s2.size();
+	} else {
+		return s1 < s2;
 	}
-	
+}
+
+int main() { // sort nhieu` tieu chi, 
+			// dong 1 : tang dan theo tu dien, dong 2 tang dan theo 
+			// chieu dai, trung` dai` thi` tang dan` theo tu` dien
+			//hoc lap trinh php
+			//^Z
+			//hoc lap php trinh
+			//hoc lap php trinh
+	string s;
+	vector<string> v;
+	while(cin >> s){
+		v.push_back(s);
+	}
+	sort(v.begin(),v.end());
+	for(string x : v) {
+		cout << x << " ";
+	}
+	cout << endl;
+	sort(v.begin(),v.end(), cmp);
+	for(string x : v) {
+		cout << x << " ";
+	}
+
 	return 0;
 }
 
