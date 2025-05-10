31cc6482a8c18decfb926e14cea8857c9e6a607a
gialongchuai <gialongchuai@gmail.com>
2025-05-10 14:24:06 +0700

    galaxy: string tao thanh string doi xung

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,19 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // 81212112312312312331823239
-			// 5
-			// => 4
-			// chi du 1 so cuc lon cho 1 so <= long long
+int main() { // kiem tra sap xep cac char trong sau co tao
+			// thanh sau doi xugn duoc hay khong
+			//3
+			//appsqss
+			//NO
+			//abbcaaa
+			//YES
+			//abbca
+			//YES
+	int n; cin >> n;
 	string s;
-	cin >> s;
-	long long m; cin >> m;
-	long long r = 0;
-	for(char x : s) {
-		r = r*10+(x-'0');
-		r%=m;
+	while(n--) {
+		cin >> s;
+		map<char,int> m;
+		for(char x : s) {
+			m[x]++;
+		}
+		int cnt_le = 0;
+		for(pair<char,int> x : m) {
+			if(x.second%2!=0) cnt_le++;
+		}
+		if(cnt_le>1) {
+			cout << "NO" << endl;
+		} else {
+			cout << "YES" << endl;
+		}
 	}
-	cout << r;
 	
 	return 0;
 }
