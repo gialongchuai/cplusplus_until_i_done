1b8111ea256e39799daca0811ec44e275f6bf80f
gialongchuai <gialongchuai@gmail.com>
2025-05-10 12:20:11 +0700

    galaxy: sort hard 2 cach string theo nhieu tieu chi

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,36 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(string s1, string s2) {
-	if(s1.size() !=s2.size()) {
-		return s1.size() < s2.size();
-	} else {
-		return s1 < s2;
+vector<string> v;
+bool cmp (string a, string b) {
+	if(a.size() != b.size()) {
+		return a.size() < b.size();
+	}else {
+		vector<string>::iterator it;
+		for(it = v.begin();it!= v.end();it++) {
+			if(*it == a) {
+				vector<string>::iterator it1;
+				for(it1 = v.begin(); it1!=v.end(); it1++) {
+					if(*it==b) return (it - v.begin()) < (it1 - v.begin());
+				}
+			}
+		}
 	}
 }
 
-int main() { // sort nhieu` tieu chi, 
-			// dong 1 : tang dan theo tu dien, dong 2 tang dan theo 
-			// chieu dai, trung` dai` thi` tang dan` theo tu` dien
-			//hoc lap trinh php
+bool cmp_02(string a, string b) {
+	return a.size() < b.size();
+}
+
+int main() { // cout dk: sap xep theo chieu dai cua chuoi thuan nghich "KHAC NHAU"
+			// trung` length thi` in theo thu tu index trong chuoi nhap ban dau`
+			//php aba aaaa nguyen aba huu hoc pop
 			//^Z
-			//hoc lap php trinh
-			//hoc lap php trinh
+			//php aba pop aaaa
 	string s;
-	vector<string> v;
-	while(cin >> s){
-		v.push_back(s);
+	set<string> se;
+	while(cin >> s) {
+		string tmp = s;
+		reverse(tmp.begin(), tmp.end());
+		if(s == tmp) {
+			if(!se.count(s)) v.push_back(s);
+			se.insert(s);
+		}
 	}
-	sort(v.begin(),v.end());
+	vector<string> bp_v = v;
+	
+	sort(v.begin(),v .end(),cmp); // cach_01 do forgot cach 02 ;)))
 	for(string x : v) {
 		cout << x << " ";
 	}
 	cout << endl;
-	sort(v.begin(),v.end(), cmp);
-	for(string x : v) {
+												// cach_02_bp
+	stable_sort(bp_v.begin(), bp_v.end(), cmp_02); // stable_sort dam bao tinh on dinh chuoi nhap
+	for(string x : bp_v) {
 		cout << x << " ";
 	}
-
+	
 	return 0;
 }
 
