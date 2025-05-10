548e7037112e58e1edc2d8ce3b82fd56f2b56d8c
gialongchuai <gialongchuai@gmail.com>
2025-05-10 15:07:07 +0700

    galaxy: string cout theo nhieu tieu chi cua 2 xau

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // kiem tra sap xep cac char trong sau co tao
-			// thanh sau doi xugn duoc hay khong
-			//3
-			//appsqss
-			//NO
-			//abbcaaa
-			//YES
-			//abbca
-			//YES
-	int n; cin >> n;
-	string s;
-	while(n--) {
-		cin >> s;
-		map<char,int> m;
-		for(char x : s) {
-			m[x]++;
-		}
-		int cnt_le = 0;
-		for(pair<char,int> x : m) {
-			if(x.second%2!=0) cnt_le++;
-		}
-		if(cnt_le>1) {
-			cout << "NO" << endl;
-		} else {
-			cout << "YES" << endl;
+int main() { // cout khong trung theo thu tu tu` dien?
+			// dong 1: chu xuat hien o ca 2 day 
+			// dong 2: chu con lai
+			// nTOhOGvRBk
+			// bUJtZSoONh
+			//	Oh
+			//	BGJNRSTUZbknotv
+	string a, b;
+	cin >> a >> b;
+	set<char> se_dk1;
+	for(int i = 0; i<a.size();i++) {
+		for(int j = 0; j<b.size(); j++) {
+			if(a[i] == b[j]) {
+				se_dk1.insert(a[i]);
+				break;
+			}	
 		}
 	}
-	
+	for(char x : se_dk1) {
+		cout << x;
+	}
+	cout << endl;
+	string z = a+b;
+	set<char> se_dk2;
+	for(int i = 0; i<z.size() ; i++) {
+		if(!se_dk1.count(z[i])) se_dk2.insert(z[i]);
+	}
+	for(char x : se_dk2) {
+		cout << x;
+	}
+		
 	return 0;
 }
 
