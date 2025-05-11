009da44f828bd7f2c54ce4dab0667ea3254c8476
gialongchuai <gialongchuai@gmail.com>
2025-05-11 14:37:10 +0700

    galaxy: string so dep voi cac chu so la snt va tong cac chu so la snt

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,26 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { 
+bool snt(int n) {
+	if(n<2) return false;
+	for(int i =2; i*i<=n; i++) {
+		if(n%i==0) return false;
+	}
+	return true;
+}
+
+int main() { // kiem tra so dep cua 1 so cuc lon
+			// chu so la snt, tong la snt
+			//373
+			// => YES	
+			//129
+			// => NO
 	string s; cin >> s;
-	s+=s[s.size()-1]; // cho 2 thang cuoi bang nhau de roi vao` else update res ;)))
-	cout << s << endl; // s+=s.back();
-	string tmp = "";
-	tmp += s[0];
-	string res = "";
-	res += s[0];
-	for(int i = 1;i<s.size(); i++) {
-		if(s[i] != s[i-1]) tmp += s[i];
-		else {
-			if(tmp.size() > res.size()) res = tmp;
-			if(tmp.size() == res.size() && tmp > res) res = tmp;
-			tmp = "";
-			tmp+=s[i];
+	int sum = 0;
+	for(char x : s){
+		int tmp = x - '0';
+		if(tmp != 2 && tmp != 3 && tmp != 5 && tmp != 7) {
+			cout << "NO";
+			return 0;
 		}
+		sum+=tmp;
 	}
 	
-	cout << res;
-		
+	if(snt(sum)) {
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+	
 	return 0;
 }
 
