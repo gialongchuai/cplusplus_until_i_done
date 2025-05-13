af91b4e0f3a48046a39c64d16a5db2f2c1169e2b
gialongchuai <gialongchuai@gmail.com>
2025-05-13 12:39:21 +0700

    galaxy: string bong da

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,30 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // cout tu theo tan xuat ke ben tu
-			// cach 01: a10c1d1u1c1u2c1d1c1a1b1c1z1u1b1d1u4b1a2
-			// => aaaaaaaaaacducuucdcabczubduuuubaa
-	string s; cin >> s;
-	for(int i = 0; i<s.size(); i++) {
-		int tmp = 1;
-		string so = "";
-		while(isdigit(s[i+tmp])) {
-			so+=s[i+tmp];
-			tmp++;
+bool cmp(pair<string,int> a, pair<string,int> b) {
+	if(a.second!=b.second) {
+		return a.second > b.second;
+	} else return a.first < b.first;
+}
+
+int main() { // sap xep tong ban thang giam dan
+			// trung` in theo thu tu tu` dien?
+			Barca 3 - 18 Man City
+			//28Tech Football Club 5 - 19 Newcastle United
+			//Chelsea 6 - 0 Newcastle United
+			//28Tech Football Club 18 - 12 Liverpool
+			//Manchester United 13 - 13 Aston Villa
+			//Arsenal 19 - 0 Aston Villa
+			//Barca 19 - 15 Man City
+			//Manchester United 19 - 6 Aston Villa
+			//Manchester United 19 - 13 Aston Villa
+			//Chelsea 15 - 3 Liverpool
+			//^Z
+			//Manchester United 51
+			//Man City 33
+			//Aston Villa 32
+			//28Tech Football Club 23
+			//Barca 22
+			//Chelsea 21
+			//Arsenal 19
+			//Newcastle United 19
+			//Liverpool 15
+	string s;
+	vector<string> v;
+	while(getline(cin,s)) {
+		stringstream ss(s);
+		string word;
+		while(getline(ss,word,'-')) {
+			v.push_back(word);
 		}
-		int x = stoi(so);
-		for(int j = 0; j<x; j++) {
-			cout << s[i];
-		}
-		i += tmp - 1;
 	}
 	
-	cout << endl;
-	
-	char c; // cach 02: a10c1d1u1c1u2c1d1c1a1b1c1z1u1b1d1u4b1a2
-	int n;
-	while(cin >> c >> n) {
-		for(int i = 0; i<n; i++) cout << c;
+	map<string,int> m;
+	for(int i = 0; i<v.size(); i++) {
+		if(i%2==0) {
+			stringstream ss(v[i]);
+			string word;
+			vector<string> tmp;
+			while(ss>>word) {
+				tmp.push_back(word);
+			}
+			string ten_db = "";
+			for(int i = 0; i<tmp.size() - 1; i++) {
+				if(i!=tmp.size()-2) {
+					ten_db += tmp[i] + " ";
+				} else {
+					ten_db += tmp[i];
+				}
+			}
+			int so_ban = stoi(tmp[tmp.size()-1]);
+			if(m.count(ten_db)) {
+				int x = m[ten_db];
+				so_ban+=x;
+				m[ten_db] = so_ban;
+			} else {
+				m.insert({ten_db,so_ban});
+			}
+		} else {
+			stringstream ss(v[i]);
+			string word;
+			vector<string> tmp;
+			while(ss>>word) {
+				tmp.push_back(word);
+			}
+			string ten_db = "";
+			for(int i = 1; i<tmp.size(); i++) {
+				if(i!=tmp.size()-1) {
+					ten_db += tmp[i] + " ";
+				} else {
+					ten_db += tmp[i];
+				}
+			}
+			int so_ban = stoi(tmp[0]);
+			if(m.count(ten_db)) {
+				int x = m[ten_db];
+				so_ban+=x;
+				m[ten_db] = so_ban;
+			} else {
+				m.insert({ten_db,so_ban});
+			}
+		}
+	}
+	vector<pair<string,int>> v_pair;
+	for(pair<string,int> x : m) {
+		v_pair.push_back(x);
+ 	}
+ 	sort(v_pair.begin(),v_pair.end(),cmp);
+	for(pair<string,int> x : v_pair) {
+		cout << x.first << " " << x.second << endl;
 	}
 	
 	return 0;
