b08a41605b32592f91f1c6a6fb8a450d1f808468
gialongchuai <gialongchuai@gmail.com>
2025-05-15 14:16:39 +0700

    galaxy: tim tong lon nhat cua ma tran 3x3 trong ma tran vuong co tong lon nhat

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,50 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int C[1005][1005];
+int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+int n;
+int a[500][500];
 
-int main() { // xay dung tam giac pascal
-		//1 		// C00
-		//1 1 		// C01 C11
-		//1 2 1		// C02 C12 C22
-		//1 3 3 1
-		//1 4 6 4 1
-		//1 5 10 10 5 1
-		//1 6 15 20 15 6 1
-		// => ung dung tinh to hop chap k cua n chia du 10^9+7
-		
-		// tv : 5
-		//9 0
-		//13 8
-		//16 7
-		//9 9
-		//7 6
-		//		result : 
-		//5
-		//9 0
-		//1
-		//13 8
-		//1287
-		//16 7
-		//11440
-		//9 9
-		//1
-		//7 6
-		//7
-	for(int i = 0; i<1000; i++) {
-		for(int j = 0; j <= i; j++) {
-			if(j==0 || i == j) C[i][j] = 1; // cot 1 va` duong` cheo chinh'
-			else {
-				C[i][j] = C[i-1][j-1] + C[i-1][j];
-				C[i][j] %= 1000000007;
-			}
+int tong_cong_don(int i,int j){
+	int tong = a[i][j];
+	for(int k = 0; k<8; k++) {
+		int di = i+dx[k];
+		int dj = j+dy[k];
+		tong+=a[di][dj];
+	}
+//	a[i][j] = tong;
+	return tong;
+}
+
+int main() { // ma tran vuong, tim ma tran 3x3 co tong lon nhat
+			//6
+			//4 6 4 5 8 2
+			//8 0 9 0 1 4
+			//3 5 3 6 3 4
+			//5 7 0 9 5 9
+			//4 5 5 9 6 3
+			//9 2 3 6 1 8
+			//
+			//42 38 39 33 40 39 36 41 37 49 46 54 40 46 44 56
+			//
+			//4 6 4 5 8 2
+			//8 0 9 0 1 4
+			//3 5 3 6 3 4
+			//5 7 0 9 5 9
+			//4 5 5 9 6 3
+			//9 2 3 6 1 8
+			// => 56
+	cin >> n;
+	for(int i = 0; i <n; i++) {
+		for(int j = 0; j < n; j++) {
+			cin >> a[i][j];
 		}
 	}
-	int q; cin >> q;
-	while(q--) {
-		int n, k; cin >> n >> k;
-		cout << C[n][k] << endl;
+	
+	cout << endl;
+	int max_val = a[1][1];
+	for(int i = 1; i <n -1; i++) {
+		for(int j = 1; j < n-1; j++) {
+			int tong = tong_cong_don(i,j);
+			cout << tong << " ";
+			if(tong > max_val) max_val = tong;
+		}
+	}
+	cout << endl << endl;
+	for(int i = 0; i <n; i++) {
+		for(int j = 0; j < n; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
 	}
 	
+	cout << max_val;
 	return 0;
 }
