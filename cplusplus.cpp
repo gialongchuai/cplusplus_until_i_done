fc357328fd322b754eb1044a6360d62434a14680
gialongchuai <gialongchuai@gmail.com>
2025-05-15 19:18:05 +0700

    galaxy: t prime voi so co 3 uoc

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -13,31 +13,22 @@ void sang() {
 	for(int i = 2 ; i*i <= 1000000; i++) { // check xem i bi danh dau la hop so chua moi tiep tuc loai bo
 		if(prime[i] == 1) { 			// boi cua i (tuc loai bo i*i) 
 			for(int j = i*i; j <= 1000000; j+=i) {
-			prime[j] = 0;
+				prime[j] = 0;
 			}
 		}
 		
 	}
 }
 
-int main() { // sang so nguyen to
-	int n, x; cin >> n;
+int main() { // 1 so co dung 3 uoc, binh phuong so nguyen to tuc la 1 uoc 1 chinh no va snt vua ^2
+	//	int n, x; cin >> n;
+			//  duoi 10
+			//4 9 25 49
 	sang();
-	bool tmp = true;
-	while(n--) {
-		cin >> x;
-		if(prime[x] == 0) {
-			tmp = false;
+	for(int i = 0; i*i<= 100; i++) {
+		if(prime[i] == 1) {
+			cout << i*i << " ";
 		}
-	}
-	if(tmp) {
-		cout << "YES";
-	} else {
-		cout << "NO";
-	}
-	cout << endl;
-	for(int i = 0; i<=1000; i++) {
-		if(prime[i] == 1) cout << i << " ";
 	}
 	
 	return 0;
