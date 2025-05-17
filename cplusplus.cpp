5ea84b71320a9da3d0dd4e89a919ecf8c374f1c9
gialongchuai <gialongchuai@gmail.com>
2025-05-17 20:37:16 +0700

    galaxy: thuat toan sinh nhi phan

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,63 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int pos[1000];
+int a[1000];
+int n;
 
-int main() { // tim so vong tang dan`
-			//8
-			//8 1 4 5 3 2 7 6
-			//5
-			//5
-			//1 2
-			//2 6
-			//3 5
-			//4 3
-			//5 4
-			//6 8
-			//7 7
-			//8 1
-			
-			//5
-			//4 2 1 5 3
-			//3
-			//3
-			//1 3
-			//2 2
-			//3 5
-			//4 1
-			//5 4
-	int n; cin >> n;
-	vector<int> v;
-	map<int,int> m;
-	for(int i = 1; i<= n; i++) {
-		int tmp; cin >> tmp;
-		m[i] = tmp;
-		v.push_back(tmp);
-		
-		// cach bp 02
-		pos[tmp] = i;
-	}
-	int cnt = 1;
-	for(int i = 2; i<n; i++) {
-		for(pair<int,int> x : m) {
-			if(x.second == i) {
-				if(x.first >= i) cnt++;
-			}
-		}
-	}
-	cout << cnt;
-	cout << endl;
-	int ans = 1;
-	for(int i = 2; i<=n; i++) {
-		if(pos[i] < pos[i-1]){
-			ans++;
-		}
-	}
-	cout << ans << endl;
-	
+void khoitao() {
+	cin >> n;
 	for(int i = 1; i<=n; i++) {
-		cout << i << " " << pos[i] << endl;
+		a[i] = 0;
 	}
+}
+
+bool check() {
+	for(int i = 1 ; i<=n; i++) {
+		if(a[i] == 0) return false;
+	}
+	return true;
+}
+
+void sinh() {
+	int i = n; 
+	while(i>1 && a[i] == 1) {
+		a[i] = 0;
+		i--;
+	}
+	a[i] = 1;
+}
+
+int main() { // thuat toan sinh nhi phan
+	khoitao();
+	while(!check()) {
+		for(int i = 1 ; i<=n; i++) {
+			cout << a[i] ;
+		}
+		cout << endl;
+		sinh();
+	}
+	for(int i = 1 ; i<=n; i++) {
+		cout << a[i] ;
+	}
+
 	return 0;
 }
 
