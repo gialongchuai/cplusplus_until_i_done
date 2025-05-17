0b4db3a51f4c5bace22f10123957642d3d4f14d4
gialongchuai <gialongchuai@gmail.com>
2025-05-17 18:52:57 +0700

    galaxy: bao nhieu vong voi so tang tu trai qua phai

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,27 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sang(long long l, long long r) {
-	int prime[r-l+1];
-	for(long long i = 0; i<=r-l; i++){
-		prime[i] = 1;
-	}
-	for(int i = 2; i*i<=r; i++) { // cong thuc tim boi dau tien lon hon l
-		for(long long j = max(1ll * i*i,(l+i-1) / i * i); j<=r; j+=i) { 
-			prime[j-l] = 0; // j-l
-		}
-	}
-	for(long long i = max(2ll, l); i<=r ;i++) {
-		if(prime[i-l] == 1) { // i - l
-			cout << i << " ";
-		}
-	}
- 	 
-}
+int pos[1000];
 
-int main() { // sang so nguyen to tren doan a b cuc lon
-	sang(10001000,10010000);
+int main() { // tim so vong tang dan`
+			//8
+			//8 1 4 5 3 2 7 6
+			//5
+			//5
+			//1 2
+			//2 6
+			//3 5
+			//4 3
+			//5 4
+			//6 8
+			//7 7
+			//8 1
+			
+			//5
+			//4 2 1 5 3
+			//3
+			//3
+			//1 3
+			//2 2
+			//3 5
+			//4 1
+			//5 4
+	int n; cin >> n;
+	vector<int> v;
+	map<int,int> m;
+	for(int i = 1; i<= n; i++) {
+		int tmp; cin >> tmp;
+		m[i] = tmp;
+		v.push_back(tmp);
+		
+		// cach bp 02
+		pos[tmp] = i;
+	}
+	int cnt = 1;
+	for(int i = 2; i<n; i++) {
+		for(pair<int,int> x : m) {
+			if(x.second == i) {
+				if(x.first >= i) cnt++;
+			}
+		}
+	}
+	cout << cnt;
+	cout << endl;
+	int ans = 1;
+	for(int i = 2; i<=n; i++) {
+		if(pos[i] < pos[i-1]){
+			ans++;
+		}
+	}
+	cout << ans << endl;
 	
+	for(int i = 1; i<=n; i++) {
+		cout << i << " " << pos[i] << endl;
+	}
 	return 0;
 }
 
