ac7cc87c21e916a7c21c034cbecff090eeac6808
gialongchuai <gialongchuai@gmail.com>
2025-05-18 20:04:23 +0700

    galaxy: dem mang con chia het cho k

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,29 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int pos[100];
-
-int main() { // dem mang con do dai` bang 7: luu luon tan suat
-			//13 7
-			//3 -2 2 0 0 1 1 2 0 0 4 3 5
-			// 3 1 3 3 3 4 5 7 7 7 11 14 19 7 => tong cong don
-			//7
+int main() { // dem mang con co tong chia het cho k
+			// tong chia du cong don value map
 	int n, k; cin >> n >> k;
 	int a[n];
-	for(int i = 0; i <n ;i++) {
+	for(int i = 0; i<n; i++) {
 		cin >> a[i];
 	}
-	map<int,int> m;
 	int sum = 0;
 	int ans = 0;
+	map<int,int> m;
 	for(int i = 0; i<n; i++) {
-		sum+=a[i];
-		if(m.count(sum-k)) ans+=m[sum-k];
-		if(sum==k) ans++;
+		sum+= a[i];
+		if(sum == k || sum == 0) ans++;
+		sum%=k;
+		if(m.count(sum)) ans += m[sum];
 		m[sum]++;
-		cout << sum << " ";
 	}
-	cout << endl << ans;
+	cout << ans;
  
 	return 0;
 }
