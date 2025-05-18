4b4de4ea63a94b662750f480321e3bbc3018730c
gialongchuai <gialongchuai@gmail.com>
2025-05-18 15:30:08 +0700

    galaxy: ghep cap co tong bang k

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // khieu vu
-	// ghep cap nam cao hon nu : cho 2 day tang dan xong xet tung nam voi nu
-	int nam, nu; cin >> nam >> nu;
-	int a[nam], b[nu];
-	for(int &x : a) cin >> x;
-	for(int &x : b) cin >> x;
-	int cnt = 0;
-	int l = 0, r = 0;
-	sort(a,a+nam);
-	sort(b,b+nu);
-	while(l < nam && r < nu) {
-		if(a[l] > b[r]) {
-			cnt++; l++; r++;
-		} else {
-			l++;
-		}
+int main() { // ghep cap co tong bang k
+	int n, k; cin >> n >> k;
+	int a[n];
+	for(int i = 0; i<n; i++) {
+		cin >> a[i];  
 	}
-	cout << cnt;
+	sort(a,a+n);
+	for(int x : a) {
+		cout << x << " ";
+	}
+	cout << endl;
+	int ans = 0;
+	for(int i = 0; i < n; i++) {
+		int tim = k - a[i];
+		int *it = lower_bound(a+i+1,a+n,tim);
+		int *it2 = upper_bound(a+i+1,a+n,tim);
+		ans += it2-it;
+	}
+	cout << ans;
+	
 
 	return 0;
 }
