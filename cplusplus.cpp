7a8bf678c5956f7a144a58760ffcc1c4abe7fa30
gialongchuai <gialongchuai@gmail.com>
2025-05-18 14:13:54 +0700

    galaxy: khieu vu nam nu

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,44 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[1000];
-int n;
-
-void khoitao() {
-	cin >> n;
-	for(int i = 1; i<=n; i++) {
-		a[i] = 0;
-	}
-}
-
-bool check() {
-	for(int i = 1 ; i<=n; i++) {
-		if(a[i] == 0) return false;
-	}
-	return true;
-}
-
-void sinh() {
-	int i = n; 
-	while(i>1 && a[i] == 1) {
-		a[i] = 0;
-		i--;
-	}
-	a[i] = 1;
-}
-
-int main() { // thuat toan sinh nhi phan
-	khoitao();
-	while(!check()) {
-		for(int i = 1 ; i<=n; i++) {
-			cout << a[i] ;
+int main() { // khieu vu
+	// ghep cap nam cao hon nu : cho 2 day tang dan xong xet tung nam voi nu
+	int nam, nu; cin >> nam >> nu;
+	int a[nam], b[nu];
+	for(int &x : a) cin >> x;
+	for(int &x : b) cin >> x;
+	int cnt = 0;
+	int l = 0, r = 0;
+	sort(a,a+nam);
+	sort(b,b+nu);
+	while(l < nam && r < nu) {
+		if(a[l] > b[r]) {
+			cnt++; l++; r++;
+		} else {
+			l++;
 		}
-		cout << endl;
-		sinh();
 	}
-	for(int i = 1 ; i<=n; i++) {
-		cout << a[i] ;
-	}
+	cout << cnt;
 
 	return 0;
 }
