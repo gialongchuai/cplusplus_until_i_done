63052d46ea84d4f319bc3ec9ce394d154f832092
gialongchuai <gialongchuai@gmail.com>
2025-05-19 17:18:06 +0700

    galaxy: thuat toan sinh to hop day con ke tiep

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -7,7 +7,7 @@ int n, k;
 void khoitao(){
 	cin >> n >> k; // C 5 cua 9 : k=5 , n=9
 	for(int i = 1; i <= k; i++) {
-		a[i] = i;
+		cin >> a[i];
 	}
 }
 
@@ -31,33 +31,17 @@ bool check() { // kiem tra co dat gia tri cuoi 5 6 7 8 9
 	return true;
 }
 
-int main() { // thuat toan sinh to hop tang dan
-			//6 4
-			//1 2 3 4
-			//1 2 3 5
-			//1 2 3 6
-			//1 2 4 5
-			//1 2 4 6
-			//1 2 5 6
-			//1 3 4 5
-			//1 3 4 6
-			//1 3 5 6
-			//1 4 5 6
-			//2 3 4 5
-			//2 3 4 6
-			//2 3 5 6
-			//2 4 5 6
-			//3 4 5 6
+int main() { // 
 	khoitao();
-	while(!check()) {
+	if(check()) {
 		for(int i = 1; i<=k; i++) {
+			cout << i << " ";
+		}
+	} else {
+		sinh();
+		for(int i = 1; i<=k ;i++) {
 			cout << a[i] << " ";
 		}
-		cout << endl;
-		sinh();
-	}
-	for(int i = 1; i <=k; i++) {
-		cout << a[i] << " ";
 	}
 
 	return 0;
