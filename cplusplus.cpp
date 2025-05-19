a393734f7b03b606f0328c5669f1d3db7ae19578
gialongchuai <gialongchuai@gmail.com>
2025-05-19 17:45:01 +0700

    galaxy: thuat toan sinh to hop dem chuoi con

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int a[500];
-int n, k;
+int n, k, final = 1;
 
 void khoitao(){
 	cin >> n >> k; // C 5 cua 9 : k=5 , n=9
 	for(int i = 1; i <= k; i++) {
-		cin >> a[i];
+		a[i] = i;
 	}
 }
 
@@ -16,33 +16,42 @@ void sinh() {
 	while(i>=1 && a[i]==n-k+i) {
 		i--; // tim phan tu dau tien chua dat gia tri max tinh tu ben phai
 	}
-	a[i] += 1;	
-	int ans = 1; // 3 5 6 => 4 5 6
-	for(int j = i + 1; j<=k; j++) {
-		a[j] = a[i] + ans;
-		ans++;
-	}
-}
-
-bool check() { // kiem tra co dat gia tri cuoi 5 6 7 8 9
-	for(int i = 1; i<=k ; i++) {
-		if((n-k+i) != a[i]) return false;
-	}
-	return true;
-}
-
-int main() { // 
-	khoitao();
-	if(check()) {
-		for(int i = 1; i<=k; i++) {
-			cout << i << " ";
-		}
+	if(i == 0) {
+		final = 0;
 	} else {
-		sinh();
-		for(int i = 1; i<=k ;i++) {
-			cout << a[i] << " ";
+			a[i] += 1;	
+		int ans = 1; // 3 5 6 => 4 5 6
+		for(int j = i + 1; j<=k; j++) {
+			a[j] = a[i] + ans;
+			ans++;
 		}
 	}
+}
+
+int main() { // dem chuoi con to hop
+	khoitao();
+	vector<vector<int>> v;
+	int stt = 0;
+	while(final) {
+		stt++;
+		vector<int> v_tamp;
+		for(int i = 1; i<=k; i++) {
+			cout << a[i];
+			v_tamp.push_back(a[i]);
+		}
+		v.push_back(v_tamp);
+		cout << endl;
+		sinh();
+	}
+	cout << endl;
+	
+	for(vector<int> x : v) {
+		for(int j : x) {
+			cout << j;
+		}
+		cout << endl;
+	}
+	cout << stt << " " << v.size();
 
 	return 0;
 }
