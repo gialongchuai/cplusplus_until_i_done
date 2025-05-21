7ff71bedc6cac609b06b67e52dfeb23da48fe5b5
gialongchuai <gialongchuai@gmail.com>
2025-05-21 11:28:11 +0700

    galaxy: leetcode add two numbers

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,74 +1,81 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 #include <bits/stdc++.h>
-using namespace std;
+class Solution { // leetcode add two numbers :(
+public:
+    string sum(string a, string b) {
+        while(a.size() < b.size()) {
+            a.insert(0,"0");
+        }
+        while(a.size() > b.size()) {
+            b.insert(0,"0");
+        }
+        int n = a.size();
+        vector<int> v;
+        int q = 0;
+        for(int i = n-1; i>=0; i--) {
+            int tong = a[i] - '0' + b[i] - '0'+ q;
+            if(tong >= 10) {
+                int r = tong%10;
+                v.push_back(r);
+                tong/=10;
+                q=tong;
+            } else {
+                v.push_back(tong);
+                q=0;
+            }
+        }
+        if(q) {
+            v.push_back(q);
+        }
+        reverse(v.begin(), v.end());
+        string s = "";
+        for(int x : v) {
+            s+=x+'0';
+        }
+        return s;
+    }
 
-int a[100];
-int n, final = 1;
-
-
-void khoitao() {
-	cin >> n;
-	for(int i = 1; i <=n; i++) {
-		a[i] = i;
-	}
-}
-
-void sinh() {
-	int i = n-1; 
-	while(i>=1 && a[i] > a[i+1]) {
-		i--;
-	}
-	if(i == 0) {
-		final = 0;
-	} {
-		int j = n;
-		while(a[i] > a[j]) j--;
-		swap(a[i], a[j]);
-		sort(a+i+1, a+n+1);
-	}
-}
-
-int main() { // thuat toan sinh hoan vi
-	khoitao();
-	while(final) {
-		for(int i = 1; i<=n; i++) {
-			cout << a[i];
-		}
-		cout << endl;
-		sinh();
-	}
-			//4
-			//1234
-			//1243
-			//1324
-			//1342
-			//1423
-			//1432
-			//2134
-			//2143
-			//2314
-			//2341
-			//2413
-			//2431
-			//3124
-			//3142
-			//3214
-			//3241
-			//3412
-			//3421
-			//4123
-			//4132
-			//4213
-			//4231
-			//4312
-			//4321
-	return 0;
-}
-
-
-
-
-
-
-
-
-
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        vector<int> v1;
+        while(l1!=NULL) {
+            v1.push_back(l1->val);
+            l1 = l1->next;
+        }
+        vector<int> v2;
+        while(l2!=NULL) {
+            v2.push_back(l2->val);
+            l2 = l2->next;
+        }
+        reverse(v1.begin(),v1.end());
+        reverse(v2.begin(),v2.end());
+        string s1 = "";
+        for(int i : v1) s1+=i + '0';
+        string s2 = "";
+        for(int i : v2) s2+=i + '0';
+        ListNode* result = new ListNode();
+        ListNode* tam = result;
+        string k = sum(s1,s2);;
+        reverse(k.begin(), k.end());
+        int p = 0;
+        for(char i : k) {
+            int temp = i - '0';
+            result->val = temp;
+            if(p!=k.size()-1) {
+                ListNode* x = new ListNode();
+                result->next = x;
+                result = result->next;
+            }
+            p++;
+        }
+        return tam; 
+    }
+};
