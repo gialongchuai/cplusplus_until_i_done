e5b578323830d90e4f46375cb02d4c0301e4853b
gialongchuai <gialongchuai@gmail.com>
2025-05-23 19:34:31 +0700

    galaxy: con chuot trong me cung

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,33 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[100];
+int a[100][100];
 int n;
 int used[100];
+bool check = true;
+string s = "";
 
-void QL(int i) { // di tu 1 den n
-	for(int j = 1; j <= n; j++) {
-		if(used[j] == 0) {
-			a[i] = j;
-			used[j] = 1; // danh dau so dung roi khong dung lai
-			if(i==n) { // dat den n in ra
-				for(int k =1 ; k<=n; k++) {
-					cout << a[k];
-				}
-				cout << endl;
-			} else {
-				QL(i+1); // quay chua toi i==n thi` tiep tuc quay (dua vao ngan xep)
-			}
-			used[j] = 0; // danh dung het trong if roi` thi` tra lai cho ngta
-						// chac chan la giai phong het con chu khong phai ca nhanh 
-						//de di sang nhanh khac j++
-		}
+void Try(int i, int j) {
+	if(i==n && j==n) {
+		cout << s << endl;
+		check = false;
+		return;
+	}
+	
+	if(i+1 <=n && a[i+1][j] == 1) {
+		s+="D";
+		Try(i+1,j);
+		s.pop_back();
+	}
+	if(j+1 <=n && a[i][j+1] == 1) {
+		s+="R";
+		Try(i,j+1);
+		s.pop_back();
 	}
 }
 
-int main() { // thuat toan sinh hoan vi
+int main() { // con chuot tu 1:1 di den n:n, neu khong di den cout -1
 	cin >> n;
-	QL(1);
+	for(int i = 1; i<=n; i++) {
+		for(int j = 1; j<=n; j++) {
+			cin >> a[i][j];
+		}
+	}
+	cout << endl;
+	Try(1,1);
+	if(check) {
+		cout << -1;
+	}
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
