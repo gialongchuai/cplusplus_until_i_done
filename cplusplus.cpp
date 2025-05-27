3cf76fc2480f28fae5784154cb5e77fe3e4b8061
gialongchuai <gialongchuai@gmail.com>
2025-05-27 09:35:42 +0700

    galaxy: de quy xau fibo 02

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -3,27 +3,37 @@ using namespace std;
 
 long long F[100];
 
-char Find(long long n, long long k) {
+string Find(long long n, long long k) {
 	cout << n << " " << k << endl;
-	if(n==1) return 'A';
-	if(n==2) return 'B';
-	if(k<=F[n-2]) {
-		return Find(n-2, k);
+	if(n==1) return "28TECH";
+	if(n==2) return "C++";
+	if(n==3) {
+		if(k==1) {
+			return "DSA";
+		} else {
+			return "JAVA";
+		}
+	}
+	if(k<=F[n-3]) {
+		return Find(n-3, k);
+	} else if(k<=F[n-2]+F[n-3]) {
+		return Find(n-2, k-F[n-3]);
 	} else {
-		return Find(n-1, k-F[n-2]);
+		return Find(n-1, k-F[n-3]-F[n-2]);
 	}
 }
 
 int main() {
-	F[1] = 0, F[1] = 1;
-	for(int i = 2; i<=92; i++) {
-		F[i] = F[i-2] + F[i-1];
+	F[1] = 1, F[2] = 1, F[3] = 2;	
+	for(int i = 4; i<=60; i++) {
+		F[i] = F[i-3] + F[i-2] + F[i-1];
 	}
 
-	// A B AB BAB
-	long long n, k;
-	cin >> n >> k;
-	cout << Find(n,k);
+	int tv; cin >> tv;
+	while(tv--) {
+		long long n, k; cin >> n >> k;
+		cout << Find(n,k) << endl;
+	}
 }
 
 
