3618fed7b79de0ca82c0b852f9156cbade50ebc1
gialongchuai <gialongchuai@gmail.com>
2025-05-29 12:48:35 +0700

    galaxy: duong di ngan nhat cua con soc voi buoc nhay k

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // bai toan con soc nhay trong mang (i+1 or i+2)
-			//9
-			//3 7 2 4 8 1 1  5  5
-			//0 4 1 3 7 6 6 10 10 : mang F chi luu duong di ngan nhat
-			//10 : smallest distance
-	int n; cin >> n;
+int main() { // bai toan con soc nhay trong mang voi buoc nhay k
+			//5 3
+			//5 1 1 4 7
+			//0 4 4 1 4
+			//4
+	int n, k; cin >> n >> k;
 	int a[n];
 	for(int i = 0; i <n; i++) {
 		cin >> a[i];
 	}
 	int F[n];
 	F[0] = 0;
-	F[1] = abs(a[1] - a[0]);
-	for(int i=2; i<n; i++) {
-		int f_1 = F[i-1] + abs(a[i] - a[i-1]); // duong di cu + chi phi hien tai toi vi tri xet
-		int f_2 = F[i-2] + abs(a[i] - a[i-2]); // duong di cu + chi phi hien tai toi vi tri xet
-		F[i] = min(f_1, f_2); // chap nhan chi duong di ngan nhat
+	for(int i=1; i<n; i++) {
+		F[i] = INT_MAX;
+		for(int j = 1; j <= k; j++) { // lui tu` 1 den k
+			if(i-j >=0) { // dam bao tinh chi truy cap >=a[0]
+				F[i] = min(F[i], F[i-j] + abs(a[i] - a[i-j]));
+			}
+		}
 	}
 	for(int x : F) {
 		cout << x << " ";
