1019dba8ba82c2631bd18a7bd805e03426fd6a21
gialongchuai <gialongchuai@gmail.com>
2025-05-29 10:48:57 +0700

    galaxy: duong di ngan nhat cua con soc i+1 i+2

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,56 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long mod = 1e9 + 7;
-
-struct matran{
-	long long a[2][2];
-	friend matran operator * (matran x, matran y) {
-		matran kq;
-		for(int i = 0; i<2; i++) {
-			for(int j = 0; j<2; j++) {
-				kq.a[i][j] = 0;
-				for(int k = 0; k <2; k++) {
-					kq.a[i][j] += (x.a[i][k]*y.a[k][j]);
-					kq.a[i][j] %= mod; // vuot qua mod moi bat dau chia du 
-				}
-			}	
-		}
-		return kq;
+int main() { // bai toan con soc nhay trong mang (i+1 or i+2)
+			//9
+			//3 7 2 4 8 1 1  5  5
+			//0 4 1 3 7 6 6 10 10 : mang F chi luu duong di ngan nhat
+			//10 : smallest distance
+	int n; cin >> n;
+	int a[n];
+	for(int i = 0; i <n; i++) {
+		cin >> a[i];
 	}
-};
-
-matran binpow(matran x, long long n) { // luy thua nhi phan => luy thua ma tran
-	if(n==1) return x;
-	matran X = binpow(x, n/2);
-	if(n%2==0) {
-		return X * X;
-	} else {
-		return X * X * x;
+	int F[n];
+	F[0] = 0;
+	F[1] = abs(a[1] - a[0]);
+	for(int i=2; i<n; i++) {
+		int f_1 = F[i-1] + abs(a[i] - a[i-1]); // duong di cu + chi phi hien tai toi vi tri xet
+		int f_2 = F[i-2] + abs(a[i] - a[i-2]); // duong di cu + chi phi hien tai toi vi tri xet
+		F[i] = min(f_1, f_2); // chap nhan chi duong di ngan nhat
 	}
-}
-
-int main() {
-	matran x;
-	x.a[0][0] = 1;
-	x.a[0][1] = 1;
-	x.a[1][0] = 1;
-	x.a[1][1] = 0;
-	
-	matran y;
-	y.a[0][0] = 1;
-	y.a[0][1] = 1;
-	y.a[1][0] = 1;
-	y.a[1][1] = 0;
-	for(int i = 1; i <=10; i++) {
-		x=x*y;
+	for(int x : F) {
+		cout << x << " ";
 	}
-	cout << x.a[0][1] << endl; // cout 89
+	cout << endl << F[n-1];
 	
-	matran tmp = binpow(y, 11); // khong truyen x do x da bi thay doi
-	cout << tmp.a[0][1]; // cout 89
-	
-	return 0;
+ 	return 0;
 }
 
 
