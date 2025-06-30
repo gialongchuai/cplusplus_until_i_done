cc8e2fc3d513e93d95c39e2262c26d7c6e52b2b6
gialongchuai <gialongchuai@gmail.com>
2025-06-30 18:40:55 +0700

    galaxy: ly thuyet do thi thuat toan dfs duyet theo chieu sau

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,54 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a[500][500];
+int dinh, canh, timkiem;
+vector<int> ke[1005];
+int visited[500];
 
-int main() { // matrix thanh ds canh va ds ke voi do thi co huong
-			//5
-			//0 0 0 1 0
-			//1 0 1 1 1
-			//1 0 0 1 1
-			//1 0 0 0 0
-			//0 1 0 1 0
-			//
-			//1 4
-			//2 1
-			//2 3
-			//2 4
-			//2 5
-			//3 1
-			//3 4
-			//3 5
-			//4 1
-			//5 2
-			//5 4
-			//
-			//1 : 4
-			//2 : 1 3 4 5
-			//3 : 1 4 5
-			//4 : 1
-			//5 : 2 4
-	int n; cin >> n;
-	for(int i=1; i<=n; i++) {
-		for(int j=1; j<=n; j++) {
-			int x; cin >> x;
-			a[i][j] = x;
+void nhap() {
+	cin >> dinh >> canh >> timkiem;
+	for(int i=1; i<=canh; i++) {
+		int x, y; cin >> x >> y;
+		ke[x].push_back(y); // do thi vo huong neu 2 ke 3 
+		ke[y].push_back(x); // thi` 3 ke 2
+	}
+	for(int i=1; i<=dinh; i++) { // thong nhat : voi moi dinh ke 
+		sort(ke[i].begin(), ke[i].end()); // sap xep tang dan vi du ds ke cua 2 : 5 3 4 => 2 : 3 4 5
+	}	
+}
+
+void DFS(int u) {
+	cout << u << " ";
+	visited[u] = 1; // tham dinh do roi` khong tham nua 
+	for(int x : ke[u]) {
+		if(visited[x] == 0){ // neu chua tham di tham 
+			DFS(x); // loang theo chieu` sau
 		}
 	}
-	cout << endl;
-	for(int i=1; i<=n; i++) {
-		for(int j=1; j<=n; j++) {
-			if(a[i][j]) cout << i << " " << j << endl;
-		}
-	}
-	cout << endl;
-	for(int i=1; i<=n; i++) {
-		cout << i << " : ";
-		for(int j=1; j<=n; j++) {
-			if(a[i][j]) cout << j << " ";
-		}
-		cout << endl;
-	}
+}
+
+int main() { // dfs : deep first search : duyet theo chieu sau cua dinh can tim
+	nhap();
+	DFS(timkiem); // bat dau duyet dinh can tim
 		
 	return 0;
 }
