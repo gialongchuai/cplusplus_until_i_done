e1fb6f7a80b2e75a63119097fd55134e338bd320
gialongchuai <gialongchuai@gmail.com>
2025-06-30 13:45:38 +0700

    galaxy: ly thuyet do thi chuyen doi danh sach ke sang danh sach canh

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,39 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> ke[1005];
-
-int main() { // ly thuyet do thi
-			// chuyen doi do thi vo huong cua danh sach canh thanh danh sach ke
-			// cout dinh ke tang dan`
-			//5 4	// 5 dinh 4 canh
+int main() { // covert danh sach ke sang danh sach canh
+			//5		// 5 dinh
+			//4 	// dinh 1 ke voi 4
+			//3 4 5 // 2 ke voi 3 4 5
+			//2 5 	// 3 ke 2 do 2 ke 3 above nen output khoi
+			//1 2
+			//2 3
+			
+			//1 4
+			//2 3
+			//2 4
 			//2 5
-			//4 1
-			//4 2
-			//4 3
-			//^Z
-			//1 : 4 	.. canh 1 ke voi 4
-			//2 : 4 5 	.. canh 2 ke voi 4 va` 5
-			//3 : 4 	...
-			//4 : 1 2 3	
-			//5 : 2
-	int dinh, canh; cin >> dinh >> canh;
-	for(int i=0; i<canh; i++) {
-		int x, y; cin >> x >> y;
-		ke[x].push_back(y);
-		ke[y].push_back(x);
-	}
-	
-	for(int i=1; i<=dinh; i++) {
-		cout << i << " : ";
-		vector<int> x = ke[i];
-		sort(x.begin(), x.end());
-		for(int y : x) {
-			cout << y << " ";
+			//3 5
+	int n; cin >> n;
+	cin.ignore();
+	string s;
+	map<int,set<int>> m;
+	for(int i = 1; i <= n; i++) {
+		getline(cin, s);
+		stringstream ss(s);
+		string word;
+		while(ss >> word) {
+			int x = stoi(word); // thay vi` dung` count coi ton tai
+			if(m.count(x)) { // co the kiem tra xem so do co lon hon i  (tuc lon hon dong hien tai)
+				bool flag = true; // neu lon hon cout ra i -> cout ra so lon hon done!
+				set<int> tmp = m[x];
+				if(tmp.count(i)) {
+					flag = false;
+				}
+				if(flag) {
+					m[i].insert(x);
+				}
+			} else {
+				m[i].insert(x);
+			}
 		}
-		cout << endl;
 	}
-	
+	for(pair<int,set<int>> x : m) {
+		set<int> s = x.second;
+		set<int>::iterator it;
+		for(it=s.begin(); it!=s.end(); it++) {
+			cout << x.first << " " << *it << endl;
+		}
+	}
+		
 	return 0;
 }
 
