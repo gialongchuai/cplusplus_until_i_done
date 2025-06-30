1316cc843a84592c2f1f1376b07404ba0b447fe5
gialongchuai <gialongchuai@gmail.com>
2025-06-30 14:57:12 +0700

    galaxy: ly thuyet do thi matrix to danh sach canh va danh sach ke

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,43 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> ke[100];
+int a[500][500], b[500][500];
 
-bool cmp(int x, int y) {
-	return x < y;
-}
-
-int main() { // convert danh sach canh sang danh sach ke
-			// do thi co huong
-			//5 10	// 5 dinh va 10 canh ke giua cac dinh
-			//4 2
+int main() { // matrix thanh ds canh va ds ke
+			//5	// 5 dinh tuong ung cho so 1 trong ma tran la ke
+			//0 1 1 1 0		
+			//1 0 0 1 1
+			//1 0 0 1 1
+			//1 1 1 0 1
+			//0 1 1 1 0
+			//	
+			//2 1		// do thi vo huong nen output 1 khong can liet ke canh con lai
+			//3 1		// liet ke co dinh cao: 1:2 vs 2:1 => choose 2:1
 			//4 1
-			//2 5
-			//3 4
-			//1 5
-			//2 3
-			//5 1
-			//2 1
-			//1 4
-			//4 5
-			
-			//1 : 4 5
-			//2 : 1 3 5
-			//3 : 4
-			//4 : 1 2 5
-			//5 : 1
-	int dinh, canh; cin >> dinh >> canh;
-	while(canh--) {
-		int x, y; cin >> x >> y;
-		ke[x].push_back(y);
+			//4 2
+			//5 2
+			//4 3
+			//5 3
+			//5 4
+			//
+			//1 : 2 3 4		// output 2: liet ke all dinh ke
+			//2 : 1 4 5
+			//3 : 1 4 5
+			//4 : 1 2 3 5
+			//5 : 2 3 4
+	int n; cin >> n;
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			int x; cin >> x;
+			a[i][j] = x;
+			b[i][j] = x;
+		}
 	}
-	for(int i=1; i<=dinh; i++) {
+	cout << endl;
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			if(a[i][j]) {
+				cout << j << " " << i << endl;
+			}
+			a[j][i] = 0; 
+		}
+	}
+	cout << endl;
+	for(int i=1; i<=n; i++) {
 		cout << i << " : ";
-		vector<int> v = ke[i];
-		sort(v.begin(), v.end(), cmp);
-		for(int x : v) {
-			cout << x << " ";
-		} 
+		for(int j=1; j<=n; j++) {
+			if(b[i][j]) {
+				cout << j << " ";
+			}
+		}
 		cout << endl;
 	}
 		
