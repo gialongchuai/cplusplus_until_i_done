45507d470a9a6099acab0c011b71e9950ae15c1a
gialongchuai <gialongchuai@gmail.com>
2025-07-01 13:41:44 +0700

    galaxy: ly thuyet do thi thuat toan dfs duyet theo chieu rong voi do thi co huong

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -10,7 +10,7 @@ void nhap() {
 	for(int i=1; i<=canh; i++) {
 		int x, y; cin >> x >> y;
 		ke[x].push_back(y);
-		ke[y].push_back(x);
+//		ke[y].push_back(x);
 	}
 	for(int i=1; i<=dinh; i++) { // mac dinh danh sach dinh ke trong ma tran ke cua tung dinh
 		sort(ke[i].begin(), ke[i].end()); // tang dan 
@@ -18,24 +18,24 @@ void nhap() {
 }
 
 void BFS(int u) {
-	queue<int> q; // tao hang doi
-	q.push(u);		// day dinh vao hang doi
-	visited[u] = 1;		// danh giau vieng tham no roi`
-	while(!q.empty()) {			// khi ma chua rong 
-		int x = q.front();		// in ra thang dau` hang` doi 
-		q.pop();		
-		cout << x << " ";
-		for(int e : ke[x]) { 	// xong tham tat ca dinh ke voi dinh dau` hang` doi 
-			if(!visited[e]) {	// them vao hang doi 
-				q.push(e);		// ouput la nhung dinh gan thang` dau` hang` doi cout ra truoc 
-				visited[e] = 1;
+	queue<int> q;
+	q.push(u);
+	visited[u] = 1;
+	while(!q.empty()) {
+		int e = q.front();
+		cout << e << " ";
+		q.pop();
+		for(int x : ke[e]) {
+			if(!visited[x]) {
+				q.push(x);
+				visited[x] = 1;
 			}
 		}
 	}
 }
 
 int main() { // bfs : breath first search : tim kiem theo chieu rong
-		//5 7 5			// 5 dinh voi 7 canh duoc ke , tim kiem 5 lan theo chieu rong
+		//5 7 5
 		//4 1
 		//5 3
 		//4 3
@@ -43,8 +43,8 @@ int main() { // bfs : breath first search : tim kiem theo chieu rong
 		//3 2
 		//4 2
 		//2 1
-		
-		//5 1 3 2 4
+		//5 1 3 2
+		// do thi co huong, khong co dinh nao vao` duoc 4 nen ouput above
 	nhap();
 	BFS(timkiem);
 	
