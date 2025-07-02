22488e3b8b8abe9220eff31da2e08f7750b83753
gialongchuai <gialongchuai@gmail.com>
2025-07-02 12:04:01 +0700

    galaxy: ly thuyet do thi thuat toan dfs x den y duoc khong

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,9 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int dinh, canh;
-vector<int> ke[1005];
+vector<int> ke[105];
 int visited[105];
+int dinh, canh;
+vector<int> danhdau;
+void DFS(int u);
+int tplt[105];
+int cnt = 0;
 
 void nhap() {
 	cin >> dinh >> canh;
@@ -12,45 +16,45 @@ void nhap() {
 		ke[x].push_back(y);
 		ke[y].push_back(x);
 	}
-	for(int i=1; i<=dinh; i++) { // mac dinh danh sach dinh ke trong ma tran ke cua tung dinh
-		sort(ke[i].begin(), ke[i].end()); // tang dan 
-	}
-}
-
-void BFS(int u) {
-	queue<int> q;
-	q.push(u);
-	visited[u] = 1;
-	while(!q.empty()) {
-		int e = q.front();
-		q.pop();
-		for(int x : ke[e]) {
-			if(!visited[x]) {
-				q.push(x);
-				visited[x] = 1;
-			}
-		}
-	}
-}
-
-int main() { // bfs : breath first search : tim kiem theo chieu rong
-		//6 3
-		//1 2
-		//2 3
-		//3 4
-		//
-		//3
-		
-		// cout : thanh phan lien thong trong do thi, do 5 6 co don duyet rong khong toi dc
-	nhap();
-	int cnt = 0;
 	for(int i=1; i<=dinh; i++) {
-		if(!visited[i]) {
+		sort(ke[i].begin(), ke[i].end());
+	}
+	for(int i=1; i<=dinh; i++) { // chay 1 lan duy nhat xet thanh phan lien thong
+		if(!visited[i]) {		// tranh danh vao` while time limit
 			++cnt;
-			BFS(i);
+			tplt[i] = cnt;
+			DFS(i);
 		}
 	}
-	cout << endl << cnt;
+	int tv; cin >> tv;
+	while(tv--) {
+		int x, y; cin >> x >> y;
+		memset(visited, 0, sizeof(visited)); // sau khi chay moi truy van cho visited thanh` 0 het
+		if(tplt[x] == tplt[y]) cout << "1" << endl; // neu bang nhau tplt tuc tu` x den dc y
+		else cout << "-1" << endl;
+	}
+}
+
+void DFS(int u) {
+	visited[u] = 1;
+	for(int e : ke[u]) {
+		if(!visited[e]) {
+			tplt[e] = cnt;
+			DFS(e);
+		}
+	}
+}
+
+int main() { // 
+	//5 3 		// 5 dinh 3 canh
+	//5 4
+	//4 1
+	//4 3
+	//3			// 3 tv : cout 1 di dc, -1 khong duoc
+	//4 5		// 1
+	//4 2		// -1
+	//3 4		// 1
+	nhap();
 	
 	return 0;
 }
