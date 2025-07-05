385a1bc97d769d399cb6e18b7d6a5c23b3dd1c53
gialongchuai <gialongchuai@gmail.com>
2025-07-05 18:44:48 +0700

    galaxy: ly thuyet do thi kiem tra do thi co phai la cay hay khong

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,17 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> ke[105], t_ke[105];
+vector<int> ke[105];
 int dinh, canh;
 int visited[105];
-stack<int> s;
 
 void nhap() {
 	cin >> dinh >> canh;
 	for(int i=1; i<=canh; i++) {
 		int x, y; cin >> x >> y;
 		ke[x].push_back(y);
-		t_ke[y].push_back(x);
+		ke[y].push_back(x);
+	}
+	for(int i=1; i<=dinh; i++) {
+		sort(ke[i].begin(), ke[i].end());
 	}
 }
 
@@ -22,62 +24,40 @@ void DFS(int u) {
 			DFS(v);
 		}
 	}
-	s.push(u);
 }
 
-void DFS2(int u) {
-	cout << u << " ";
-	visited[u] = 1;
-	for(int v : t_ke[u]) {
-		if(!visited[v]) {
-			DFS2(v);
-		}
-	}
-}
-
-int main() {	// do thi lien thong manh neu chon random 2 dinh trong do thi luon co road
-			// thanh phan lien thong manh tuc la do thi khong lien thong manhc chia thanh nhieu
-			// thanh` phan` lien thong manh
-			// neu dem ma thanh phan lien thong manh == 1 tuc la` do thi lien thong manh\
-			
-	// Kosaraju
-	nhap();
-	for(int i=1; i<=dinh; i++) {
-		if(!visited[i]) {
-			DFS(i);
-		}
-	}
-	
-	memset(visited, 0, sizeof(visited));
-	while(!s.empty()) {
-		int x = s.top(); s.pop();
-		if(!visited[x]) {
-			DFS2(x);
-			cout << endl;
-		}
-	}
-	//8 9	// 8 dinh voi 9 canh ke cho do thi co huong
+int main(){ // do thi co phai la 1 cay 
+			// canh != dinh - 1 and phai lien thong 
+	//4 3
 	//1 2
 	//2 3
-	//3 8
-	//8 1
-	//3 4
+	//3 1
+	//Khong phai cay
+	
+	//5 4
+	//1 2
+	//2 3
+	//2 4
 	//4 5
-	//5 6
-	//6 7
-	//7 5
+	//La cay
+	nhap();
 	
-	//		output : 3 thanh` phan` lien thong manh		notice : > 1 nen 
-	//1 8 3 2							khong phai la` do` thi lien thong manh
-	//4
-	//5 7 6
+	if(canh != dinh - 1) {
+		cout << "Khong phai cay";
+		return 0;
+	}
 	
- 	return 0;
+	DFS(1); // bat dau duyet tu` 1 neu la` cay thi` visited == 1 het trong lan dau` duyet
+	for(int i=1; i<=dinh; i++) {
+		if(!visited[i]) {
+			cout << "Khong phai cay";
+			return 0;
+		}
+	}
+	cout << "La cay";
+	
+	return 0;
 }
-
-
-
-
 
 
 
