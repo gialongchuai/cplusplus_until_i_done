848236b89a1c53290c6f952972a404dcf3f7e890
gialongchuai <gialongchuai@gmail.com>
2025-07-08 11:32:13 +0700

    galaxy: ly thuyet do thi : dijkstra duong di ngan nhat toi cac dinh con lai khi xuat phat tu dinh cho truoc

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,91 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct dscanh {
-	int x, y, w;
-};
-
 int dinh, canh;
-vector<int> ke[105];
-int visited[105], parent[105], sz[105];
-vector<dscanh> ds_canh;
-
-bool cmp(dscanh a, dscanh b) {
-	return a.w < b.w;
-}
+vector<pair<int,int>> ke[105];
+int d[105]; // d de luu trong so duong di ngan nhat xuat phat tu s
 
 void nhap() {
 	cin >> dinh >> canh;
 	for(int i=1; i<=canh; i++) {
 		int x, y, w; cin >> x >> y >> w;
-		ds_canh.push_back({x,y,w}); // cau truc tu` x toi y voi trong so w
+		ke[x].push_back({y,w}); // do thi co huong nen khong push nguoc // neu vo huong thi push nguoc
 	}
-	sort(ds_canh.begin(), ds_canh.end(), cmp); // sap xep lai cho w tang dan de tim cay khung voi trong so nho nhat
 }
 
-void init() {
+void dijkstra(int s) { // bat dau voi dinh dau tien
 	for(int i=1; i<=dinh; i++) {
-		parent[i] = i;
-		sz[i] = 1; // cho so cay con moi khi gop , ban dau` bang` 1 het
+		d[i] = 1e9; // cho tat ca tai cac dinh co duong di MAX
 	}
-}
-
-int find(int u) {
-	if(u != parent[u]) // trong luc tim` thang` cao nhat thi cat nhat nhung thang con cho thang` cha luon
-		parent[u] = find(parent[u]);
-	return parent[u]; // neu u == parent thi` return thang cha cao nhat
-}
-
-int find_02(int u) {
-	while(u != parent[u]) {
-		u = parent[u];
-	}
-	return u;
-}
-
-bool union_func(int v, int u) {
-	int x = find(v); // tim thang cha cao nhat
-	int y = find(u);
-	if(x == y) return false; // da cung thuoc 1 thanh phan lien thong
+	// do bat dau` tu` s nen tai s chua di thi` set = 0
+	d[s] = 0;
 	
-	// gan cay nho hon vao cay lon hon
-	if(sz[x] < sz[y]) { // bat dau` gop lan` 01 : mac dinh nhay? vao` else 
-		parent[x] = y;
-		sz[y] += sz[x];
-	} else { // lay thang co cay con nho nhat cho no lam cha
-		parent[y] = x;
-		sz[x] += sz[y]; // tang so cay con cho thang cha
-	}
+	// tao hang doi de luu cac {trong_so, dinh} va sort trong_so tang dan de lay duong di min di truoc
+	priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
 	
-	return true;
-}
-
-void kruskal() {
-	vector<dscanh> kq;
-	int tong_trong_so_min = 0;
-	for(dscanh i : ds_canh) {
-		if(tong_trong_so_min == dinh - 1) break; // neu tong_trong_so_min != dinh - 1 tuc la khong tao duoc cay khung
-		if(union_func(i.x, i.y)) { // cay khung nhung khong dc tao thanh chu trinh`
-			tong_trong_so_min+=i.w; // tuc la them canh vao cay khung nhung canh do khong duoc tao thanh chu trinh`
-			kq.push_back(i);
+	// day diem bat dau vao hang` doi sau do tim cac dinh ke voi dinh bat dau`
+	q.push({0,s}); // {trong_so,dinh}
+	while(!q.empty()) {
+		pair<int,int> tmp = q.top(); q.pop();
+		int dis = tmp.first, u = tmp.second; // trong_so ; dinh
+		
+		// 0 > 0 => no nen bo qua if nay` lan` dau`
+		if(dis > d[u]) continue; // neu trong_so > trong_so cu thi` di tiep; 
+						// tuc la lay hang doi ra xet neu xet lai dinh di roi` ma` dis lon hon thi khong tinh duogn di do
+		// neu nho hon xet cac dinh ke tuc la` tinh` duong` di tu` dinh do'
+		for(pair<int,int> v : ke[u]) { // 1: {2,1} {3,5} voi 2 3 la` dinh` con` 3 5 la trong_so
+			int x = v.first, y = v.second; // x: dinh , y: trong so
+			
+			// neu khoang canh tai dinh ke` lon hon tuc la xet vo cuc > khoang canh dinh cu + do dai` toi dinh hien tai thi` update
+			if(d[x] > d[u] + y) {
+				d[x] = d[u] + y;
+				q.push({d[x], x});
+			}
 		}
-	}
-	cout << tong_trong_so_min << endl;
-	for(dscanh p : kq) {
-		cout << p.x << " " << p.y << " " << p.w << endl;
+		
 	}
 }
 
 int main() {	// disjoint set union => kruskal : tim cay khung (duong di toi all dinh with w minest)
 	nhap();
-	init();
-	cout << endl;
-	for(dscanh i : ds_canh) {
-		cout << i.x << " " << i.y << " " << i.w << endl;
+	dijkstra(1);
+	
+	for(int i=1; i<=dinh; i++) {
+		cout << d[i] << " ";
 	}
-	kruskal();
-
-    return 0;
+	//6 9 		// 6 dinh voi 9 canh duong di co huong kem` trong so
+	//1 2 1
+	//1 3 5
+	//2 5 1
+	//2 3 2
+	//2 4 3
+	//3 4 2
+	//4 5 4
+	//4 6 12
+	//5 6 7
+	//0 1 3 4 2 9 		// duong di ngan nhat toi cac dinh con` lai khi xuat phat tu` dinh so 1
+	return 0;
 }
 
 
