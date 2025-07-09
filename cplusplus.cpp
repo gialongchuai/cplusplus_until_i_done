d605cd3577fc1628b72680a80422671c96d91dea
gialongchuai <gialongchuai@gmail.com>
2025-07-09 18:44:28 +0700

    galaxy: stack : tim phan tu dau tien ben phai lon hon

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,29 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string solve(string s) {
-	stack<string> st;
-	for(int i=s.size()-1; i>=0; i--) {
-		if(isalpha(s[i])) {
-			string tmp = string(1, s[i]);
-			st.push(tmp);
+int main() {	// voi moi phan tu trong man cout phan tu nho hon dau tien cua phan tu lon hon dau tien ben phai
+		//7
+		//10 3 4 2 5 3 10
+		//-1 4 5 5 10 10 -1
+	int n; cin >> n;
+	int a[n];
+	for(int i=0; i<n; i++) {
+		cin >> a[i];
+	}
+	int res[n];
+	stack<int> st;
+	for(int i=0; i<n; i++) {
+		if(st.empty()) { // xu ly phan tu dau` tien, hoac bo cai if nay van dung do push i la` sure to do
+			st.push(i);
 		} else {
-			string top1 = st.top();
-			st.pop();
-			string top2 = st.top();
-			st.pop();
-			string re = top1 + top2 + s[i];
-			st.push(re);
+			while(!st.empty() && a[st.top()] < a[i]) { // da tim` dc phan tu dau` tien thi con mac dinh bang cha da tim`
+				res[st.top()] = a[i];
+				st.pop();
+			}
+			st.push(i); // them thang` hien tai vao de lat xu ly tiep
 		}
 	}
-	return st.top();
-}
-
-int main() {	// prefix to suffix : chuyen doi tu tien to sang hau to
-		//	*+AB-UV
-		//	=> AB+UV-*
-	string s; cin >> s;
-	cout << solve(s);
+	
+	while(!st.empty()) { // them vao` ma chua tim` dc thi` cho = -1
+		res[st.top()] = -1;
+		st.pop();
+	}
+	
+	for(int i=0; i<n; i++) {
+		cout << res[i] << " ";
+	}
 	
 	return 0;
 }
