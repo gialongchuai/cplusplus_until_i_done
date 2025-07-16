b3139947922f800f06e58766c4953c7ed0e008e4
gialongchuai <gialongchuai@gmail.com>
2025-07-16 17:40:21 +0700

    galaxy: codeforces_1703D

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -6,45 +6,27 @@ int main() {
     while(n--) {
         int c; cin >> c;
         map<string,int> m;
-        string x[c];
-        string y[c];
-        string z[c];
+        vector<string> v;
         for(int i=0; i<c; i++) {
             string s; cin >> s;
+            v.push_back(s);
             m[s]++;
-            x[i] = s;
         }
-        for(int i=0; i<c; i++) {
-            string s; cin >> s;
-            m[s]++;
-            y[i] = s;
+        for(string s : v) {
+            bool check = false;
+            for(int i=1; i<s.size(); i++) {
+                string a = s.substr(0,i);
+                string b = s.substr(i, s.size());
+                if(m.count(a) && m.count(b)) {
+                    check = true;
+                    break;
+                }
+            }
+            if(check) cout << "1";
+            else cout << "0";
         }
-        for(int i=0; i<c; i++) {
-            string s; cin >> s;
-            m[s]++;
-            z[i] = s;
-        }
-        int ans = 0;
-        for(string e : x) {
-            if(m[e] == 1) ans+=3;
-            else if(m[e] == 2) ans+=1;
-            else ans+=0;
-        }
-        cout << ans << " "; ans = 0;
-        for(string e : y) {
-            if(m[e] == 1) ans+=3;
-            else if(m[e] == 2) ans+=1;
-            else ans+=0;
-        }
-        cout << ans << " "; ans = 0;
-        for(string e : z) {
-            if(m[e] == 1) ans+=3;
-            else if(m[e] == 2) ans+=1;
-            else ans+=0;
-        }
-        cout << ans << " " << endl;
+        cout << endl;
     }
 
     return 0;
 }
-
