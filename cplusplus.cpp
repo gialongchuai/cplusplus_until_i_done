6ec8ea0649c7c70c70b29e66776d550d58509184
gialongchuai <gialongchuai@gmail.com>
2025-07-16 20:44:07 +0700

    galaxy: 2 con tro dem xem co bao nhieu day con co tong lon hon s

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { // 2 con tro : dem tong con lien tiep nho hon hoac bang s
-	//5 5
-	//1 2 3 4 5
-	//1 3 5 6 7
-	//7
+int main() { // 2 con tro : dem tong con lien tiep lon hon s
+	//5 7
+	//1 2 3 4 5  		=> 1234 12345 234 2345 345 45
+	//
+	//6
 	int n, s; cin >> n >> s;
 	int a[n+5];
 	for(int i=1; i<=n; i++) {
@@ -15,12 +15,11 @@ int main() { // 2 con tro : dem tong con lien tiep nho hon hoac bang s
 	int l = 1;
 	for(int r=1; r<=n; r++) {
 		sum+=a[r];
-		while(sum > s && l <= r) { // lon hon xoa bot con
-			sum-=a[l]; // loai bo nhung thang dau tien da tinh, tang de tinh day con vuot nguong s tiep theo
+		while(sum > s && l <= r) { 
+			ans+=(n-r+1);
+			sum-=a[l];
 			l++;
 		}
-		ans += (r-l+1); // nho hon thi tinh tap con
-		cout << ans << " ";
 	}
 	cout << endl << ans << endl;
 
