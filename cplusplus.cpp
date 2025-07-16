8b2efb3989b1f2e7bb9f8bccee89d719bb11e317
gialongchuai <gialongchuai@gmail.com>
2025-07-16 10:29:33 +0700

    galaxy: codeforces_1703B

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int main() {
-	cout << "Hello world!";
-	
-	return 0;
+    int n; cin >> n; 
+    while(n--) {
+        int c; cin >> c;
+        string s; cin >> s;
+        map<char,int> m;
+        for(char x : s) {
+            if(m.count(x) == 0) {
+                m[x]+=2;
+            } else {
+                m[x]+=1;
+            }
+        }
+        int ans = 0;
+        for(auto x : m) {
+            ans += x.second;
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
 }
