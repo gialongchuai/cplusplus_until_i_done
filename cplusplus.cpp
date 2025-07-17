d84ff353005b8003c821fef608ed2b425be36f93
gialongchuai <gialongchuai@gmail.com>
2025-07-17 20:03:35 +0700

    galaxy: prefix sum map tim day con dai nhat co tong bang 0

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,44 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(string a, string b) {
-	return a.size() < b.size();
-}
+int a[500];
 
-int main() { // dem sau con co day du ki tu khac nhau va` ngan nhat
-	//abcaaaabcad
-	//4
-	//bcad abcad aabcad aaabcad aaaabcad caaaabcad bcaaaabcad abcaaaabcad => cout v[0]
-	string s; cin >> s;
-	set<char> se;
-	for(char x : s) {
-		se.insert(x);
+int main() { // day con dai` nhat co tong = 0;
+	//8
+	//-2 2 -8 1 7 -10 10 23
+	//7 => -2 2 -8 1 7 -10 10
+	
+	//9
+	//15 -2 2 -8 1 7 -10 10 23
+	//7 => -2 2 -8 1 7 -10 10
+	
+	//10
+	//15 -2 2 -8 1 7 -10 10 0 23
+	//8
+	int n; cin >> n;
+	for(int i=1; i<=n; i++) {
+		cin >> a[i];
 	}
-	int l = 0;
-	vector<string> v;
-	map<char, int> m;
-	int ans = 1e9;
-	int cnt = 0;
-	string result = "";
-	for(int r=0; r<s.size(); r++) {
-		result += s[r];
-		m[s[r]]++; // tang so luong ki tu xuat hien
-		if(m[s[r]] == 1) cnt++; // lon hon 1 van khong tinh do da tinh bang 1 roi`
-		while(cnt == se.size()) {
-			v.push_back(result);
-			result.erase(0,1); // left tang tinh tien nen xoa 0,1
-			ans = min(ans, r-l+1); // chieu dai xau thoa
-			m[s[l]]--;
-			if(m[s[l]] == 0) cnt--; // bang 0 tuc thieu se.size() != cnt
-			l++;
+	
+	int sum = 0;
+	int len = 0;
+	map<int,int> m;
+	for(int i=1; i<=n; i++) {
+		sum+=a[i];
+		
+		// tong tat ca phan tu den day = 0 tuc la i phan tu == 0 do luu i begin 1
+		if(sum == 0) {
+			len=i;
+		}
+		
+		if(m.find(sum) != m.end()) { // neu da xuat hien tong giong truoc do
+			len = max(len, i-m[sum]); // thi` chi so hien tai - chi so tong xuat hien truoc do bang do dai` sau s == 0				
+		} else {
+			m[sum] = i; // luu tong xuat hien tai chi so thu i
 		}
 	}
-	cout << ans << endl;
-	sort(v.begin(), v.end(),cmp); // sort ngan
-	for(string x : v) cout << x << " ";
-
-    return 0;
+	cout << len << endl;
+	
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
