17e64b8005cfcc9e986d3aec2167b7b64e8c31ae
gialongchuai <gialongchuai@gmail.com>
2025-07-19 09:37:46 +0700

    galaxy: codeforces_2110A

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,35 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() { // dem chuoi con day nhat lien nhau voi so luong 0 == 1
-	//111001010
-	//8 => 8 ki tu cuoi co 4 so 0 va 4 so 1
-	string s; cin >> s; 
-	int n = s.size();	// convert 0 => -1 : bai toan thanh dem day con co tong bang 0 dai` nhat ;))
-	int a[n];
-	for(int i=0; i<n; i++) a[i] = s[i] - '0';
-	for(int &x : a) if(x == 0) x=-1;
+int main() {
+    int n; cin >> n;
+    while(n--) {
+        int c; cin >> c;
+        int a[c];
+        for(int i=0; i<c; i++) {
+            cin >> a[i];
+        }
+        sort(a,a+c);
 
-	map<int,int> m; // map luu tru tong xuat hien tai vi tri i
-	int ans = 0, sum = 0;
-	for(int i=0; i<n; i++) {
-		sum+=a[i]; // tong cong don` vao`
-		if(sum == 0) ans = i+1; // neu cong don` ma` tai vi tri == 0 tuc nguyen day chay tu` 1 toi i +== 0
-		if(m.find(sum) != m.end()) { // da tim thay tong cong tru lai xuat hien tong tuc day con tong == 0 hay noi cach khac nguyen day co so luong 0 va 1 bang nhau
-			ans = max(ans, i - m[sum]);
-		} else {
-			m[sum] = i; // luu vi tri dau tien xuat hien tong
-		}
-	}
-	cout << ans;
+        int l = a[0], r = a[c-1];
+        if((l+r) % 2 == 0) {
+            cout << "0" << endl;
+            continue;
+        }
+        int ans1 = 0, ans2 = 0;
+        for(int x : a){
+            if((x+r) % 2 != 0) ans1++;
+            if((x+r) % 2 == 0) break;
+        }
+        for(int i = c-1; i >= 0; i--){
+            if((a[i]+l) % 2 != 0) ans2++;
+            if((a[i]+l) % 2 == 0) break;
+        }
+        int ans = 0;
+        if(ans1!=0 && ans2!=0) {
+            ans = min(ans1,ans2);
+        } else if(ans1==0) {
+            ans = ans2;
+        } else {
+            ans = ans1;
+        }
+        cout << ans;
+        cout << endl;
+    }
 
-	
-	return 0;
+    return 0;
 }
-
-
-
-
-
-
 
