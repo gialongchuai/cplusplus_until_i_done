b9d346141afa9aed56d36a0ce8354fae0fd3a40c
gialongchuai <gialongchuai@gmail.com>
2025-08-13 16:30:47 +0700

    galaxy: de quy sinh tat ca tap con

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,42 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(int a[], int low, int high) {
-	int i = low - 1;
-	int pivot = a[high];
-	for(int j=low; j<high; j++) {
-		if(a[j] <= pivot) {
-			i++;
-			swap(a[i],a[j]);
+int n;
+vector<int> v;
+
+void solve(int k) {
+	if(k==n) {
+		cout << "{";
+		int end = v.size();
+		for(int i=0; i<end; i++) {
+			cout << v[i];
+			if(i>=0 & i<end-1) cout << ",";
 		}
-	}
-	swap(a[i+1], a[high]);
-	return i+1;
-}
-
-void quick_sort(int a[], int low, int high) {
-	if(low<high) {
-		int pii = partition(a, low, high);
+		cout << "} ";
+	} else {
+		solve(k+1);
+		v.push_back(k);
+		solve(k+1);
+//		v.push_back(k);
 		
-		quick_sort(a,low, pii-1);
-		quick_sort(a,pii+1, high);
+		v.pop_back();
 	}
 }
 
-int main() {
-	//10
-	//3 2 7 0 1 8 7 6 9 4
-	//0 1 2 3 4 6 7 7 8 9
-	//
-	//10
-	//3 2 5 0 1 8 7 6 9 4
-	//0 1 2 3 4 5 6 7 8 9
-	int n; cin >> n;
-	int a[n];
-	for(int &i : a) cin >> i;
-	quick_sort(a,0, n-1);
-	
-	for(int x : a) cout << x << " ";
+int main() { // generate subarr
+	cin >> n;
+	solve(0);
 	
 	return 0;
 }
