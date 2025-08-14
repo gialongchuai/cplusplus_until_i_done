be9f389ce2f9bb64f7e4a79c5f1326d4525f3c3e
gialongchuai <gialongchuai@gmail.com>
2025-08-14 11:29:54 +0700

    galaxy: backtrack quay lui sinh tat ca hoan vi

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,26 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(int n){
-	for(int i=0; i<(1<<n); i++) {
-		cout << "{";
-		vector<int> v;
-		for(int j=0; j<n; j++) {
-			if(1&(i>>j)) {
-				v.push_back(j);
-			}
+vector<int> permutation;
+int n;
+int a[105];
+int chosen[105];
+
+void solve() {
+	if(permutation.size() == n) {
+		for(int x : permutation) cout << x << " ";
+		cout << "\n";
+	} else {
+		for(int i=0; i<n; i++) {
+			if(chosen[i]) continue;
+			
+			chosen[i] = 1;
+			permutation.push_back(a[i]);
+			solve();
+			permutation.pop_back();
+			chosen[i] = 0;
 		}
-		for(int k=0; k<v.size(); k++)  {
-			cout << v[k];
-			if(k>=0 && k<v.size()-1) cout << ",";
-		}
-		cout << "}";
 	}
 }
 
-int main() {
-	int n; cin >> n;
-	solve(n);
+int main() { // sinh hoan vi 
+	//	3
+	//5 8 7
+	//5 8 7
+	//5 7 8
+	//8 5 7
+	//8 7 5
+	//7 5 8
+	//7 8 5
+	cin >> n;
+	for(int i=0; i<n; i++) cin >> a[i];
+	solve();
 	
 	return 0;
 }
