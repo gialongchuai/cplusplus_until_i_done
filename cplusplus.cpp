aacf0907528e9757a9d2724d24f812934d933eb9
gialongchuai <gialongchuai@gmail.com>
2025-08-16 15:26:23 +0700

    galaxy: dp day con tang dai nhat khong lien tiep dai nhat

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -2,21 +2,27 @@
 using namespace std;
 
 int main() {
-	int n, s; cin >> n >> s; // so luong coin va can` dat gia tri s ton it dong xu nhat
-	int coins[n]; // co the su dung nhieu dong xu
-	vector<int> dp(s+1, 1e9); // dp[i]=x : can ton IT NHAT x dong xu de tao ra gia tri i
-	for(int i=0; i<n; i++) cin >> coins[i];
+	//8
+	//10 9 2 5 3 7 101 1
 	
-	dp[0] = 0; // can ton it nhat 0 dong xu hay ns 0 khong can dong xu
-	for(int i=1; i<=s; i++) { // nao tao thanh gia tri 0 -> sure to andotut
-		for(int c=0; c<n; c++) {
-			int coin = coins[c]; // voi moi dong xu hop le
-			if(i>=coin && dp[i-coin]!=1e9) // co the chon dong xu nay` khong khi i>=coin tuc gia tri xet >= coin moi chon duoc chu'
-				dp[i] = min(dp[i], dp[i-coin] + 1); // neu chon dong xu thi` +1 dong xu hien tai va
-		}								// cong them toi thieu dong xu da tinh tai dp[i-coin] 
+	//1 1 1 2 2 3 4 1 => cout 4 la day con dai` nhat : 2 3 7 101
+	
+	//	1 2 5 8
+	//	1 2 3 1?	=> so 1 do se tinh nhu sau: (lay d[3] = max(1 : 1+1 : 1+2 : 1+3) lay max)
+	
+	int n; cin >> n;
+	int a[n];
+	for(int &i : a) cin >> i;
+	vector<int> dp(n, 1); // voi moi phan tu ton tai do dai chinh no la 1
+	
+	for(int i=1; i<n; i++) {
+		for(int j=0; j<i; j++) {
+			if(a[j] < a[i])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
 	}
-	if(dp[s] == 1e9) cout << -1;
-	else cout << dp[s];
+//	sort(dp.begin(), dp.end());
+	for(int x : dp) cout << x << " ";
 	
 	return 0;
 }
