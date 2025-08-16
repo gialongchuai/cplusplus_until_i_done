18eb5b887986bf03e52dfbf81b1d8d505101f8fa
gialongchuai <gialongchuai@gmail.com>
2025-08-16 13:55:38 +0700

    galaxy: dp so dong xu toi thieu tao thanh gia tri s

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,53 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n = 8;
-int chinh[500], phu[500], col[500];
-int ans = 0; // dem so cach thoa
-int d[105];
-
-char c[500][500];
-
-void printArr() {
-	for(int row=0; row<n; row++) {
-		for(int col=0; col<n; col++) {
-			if(d[row] == col) cout << "Q";
-			else cout << ".";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-
-void solve(int k) {
-	if(k==n) {
-		printArr();
-		ans++;
-		return;
-	} 
-	for(int j=0; j<n; j++) {
-		if(col[j] == 1 || chinh[k-j+n]==1 || phu[k+j]== 1) continue;
-		
-		d[k]=j; // dat con hau len ban co khong can backtrack do se dc ghi de`
-		col[j] = 1;
-		chinh[k-j+n] = 1;
-		phu[k+j]=1;
-		
-		solve(k+1);
-		
-		col[j] = 0;
-		chinh[k-j+n] = 0;
-		phu[k+j]=0;
-	}
-}
-
-
 int main() {
-	// ma tran 8*8 co 92 ban` co dat hau de bat ki khong co hau nao` an nhau;
-	// hau an theo hang cot va` 2 duong cheo
-	// do duyet theo tung hang nen chi can` xet 3 yeu to con` lai: cot, chinh and phu
-	solve(0);
-	cout << ans;
-	 
+	int n, s; cin >> n >> s; // so luong coin va can` dat gia tri s ton it dong xu nhat
+	int coins[n]; // co the su dung nhieu dong xu
+	vector<int> dp(s+1, 1e9); // dp[i]=x : can ton IT NHAT x dong xu de tao ra gia tri i
+	for(int i=0; i<n; i++) cin >> coins[i];
+	
+	dp[0] = 0; // can ton it nhat 0 dong xu hay ns 0 khong can dong xu
+	for(int i=1; i<=s; i++) { // nao tao thanh gia tri 0 -> sure to andotut
+		for(int c=0; c<n; c++) {
+			int coin = coins[c]; // voi moi dong xu hop le
+			if(i>=coin && dp[i-coin]!=1e9) // co the chon dong xu nay` khong khi i>=coin tuc gia tri xet >= coin moi chon duoc chu'
+				dp[i] = min(dp[i], dp[i-coin] + 1); // neu chon dong xu thi` +1 dong xu hien tai va
+		}								// cong them toi thieu dong xu da tinh tai dp[i-coin] 
+	}
+	if(dp[s] == 1e9) cout << -1;
+	else cout << dp[s];
+	
 	return 0;
 }
