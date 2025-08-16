0129362ca4174989e2deb29897ec782aa569f4e8
gialongchuai <gialongchuai@gmail.com>
2025-08-16 19:30:46 +0700

    galaxy: bp dp knapsack

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -10,34 +10,21 @@ int main() {
 	//2 3 4 5
 	//3 4 5 6
 	//10
-	//4 2
 	
-	int dp[105][105] = {0};
+	int dp[105] = {0};
 	
 	int n, W; cin >> n >> W;
 	int w[n+1] = {0}, v[n+1] = {0};
 	for(int i=1; i<=n; i++) cin >> w[i];
 	for(int i=1; i<=n; i++) cin >> v[i];
 	
-	for(int j=1; j<=W; j++) {
-		dp[0][j]=0;
-	}
-	
 	for(int i=1; i<=n; i++) {
-		for(int j=0; j<=W; j++) {
-			dp[i][j] = dp[i-1][j];
-			if(j>=w[i]) dp[i][j] = max(dp[i][j], dp[i-1][j-w[i]]+v[i]);
+		int we = w[i];
+		for(int j=W; j>=we; j--) {
+			dp[j] = max(dp[j], dp[j-w[i]]+v[i]);
 		}
 	}
-	cout << dp[n][W] << endl;
-	
-	int j = W;
-	for(int i=n; i>=1; i--) {
-		if(dp[i][j] != dp[i-1][j]) {
-			cout << i << " ";
-			j-=w[i];
-		}
-	}
+	cout << dp[W];
 	
 	return 0;
 } 
